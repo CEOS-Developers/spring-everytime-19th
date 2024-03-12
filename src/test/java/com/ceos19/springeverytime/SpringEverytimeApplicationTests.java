34f21359b605628014c4b89d9bb51e604c9c6a2d
kwon <kckc0608@naver.com>
2024-03-12 23:20:42 +0900

    feat: init project

---
+++ src/test/java/com/ceos19/springeverytime/SpringEverytimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ceos19.springeverytime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEverytimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
