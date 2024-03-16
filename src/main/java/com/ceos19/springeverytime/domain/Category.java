04dccbf64c58e375441c3e989bde43dd05250050
kwon <kckc0608@naver.com>
2024-03-16 11:07:41 +0900

    feat: Category 엔티티 작성

---
+++ src/main/java/com/ceos19/springeverytime/domain/Category.java
@@ -0,0 +1,23 @@
+package com.ceos19.springeverytime.domain;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+public class Category {
+    @Id
+    private Long id;
+
+    @Column(length = 100, nullable = false)
+    private String name;
+
+    @Column(length = 300, nullable = false)
+    private String description;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createDate;
+
+    @OneToOne
+    private User manager;
+}
