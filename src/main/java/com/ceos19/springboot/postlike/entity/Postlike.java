a8f95b77ab2b4e617cd556f01d6b21ea7d18925d
sunghyun1356 <sunghyun1356@naver.com>
2024-03-16 20:33:43 +0900

    [FEAT] PostLike Entity Create

---
+++ src/main/java/com/ceos19/springboot/postlike/entity/Postlike.java
@@ -0,0 +1,13 @@
+package com.ceos19.springboot.postlike.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "post_like")
+public class Postlike {
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    private Long postLikeId;g
+
+
+}
