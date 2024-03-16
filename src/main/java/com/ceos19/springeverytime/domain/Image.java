f6610465b26c7b59e65ba32011922e88115e06e6
kwon <kckc0608@naver.com>
2024-03-16 11:17:09 +0900

    feat: Image 엔티티 작성

---
+++ src/main/java/com/ceos19/springeverytime/domain/Image.java
@@ -0,0 +1,16 @@
+package com.ceos19.springeverytime.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+public class Image {
+    @Id
+    private Long id;
+
+    @Column(length = 200)
+    private String imageUrl;
+
+    @OneToMany
+    private Post post;
+}
