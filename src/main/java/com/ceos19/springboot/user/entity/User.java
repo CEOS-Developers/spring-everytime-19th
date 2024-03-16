129963defdb6f06e92cf58ad4641844daffeb610
sunghyun1356 <sunghyun1356@naver.com>
2024-03-16 21:00:55 +0900

    [FEAT] User Entity Create

---
+++ src/main/java/com/ceos19/springboot/user/entity/User.java
@@ -0,0 +1,26 @@
+package com.ceos19.springboot.user.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Getter
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(nullable = false, length =20)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private UserRoleEnum role;
+
+    @Column(nullable = false)
+    private String nickName;
+}
