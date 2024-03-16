347a783020eb907035abb9e12511f4d2619b13c1
sunghyun1356 <sunghyun1356@naver.com>
2024-03-16 20:23:22 +0900

    [FEAT] Lecture Entity Create

---
+++ src/main/java/com/ceos19/springboot/lecture/entity/Lecture.java
@@ -0,0 +1,33 @@
+package com.ceos19.springboot.lecture.entity;
+
+import com.ceos19.springboot.common.BaseEntity;
+import jakarta.persistence.*;
+
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "lecture")
+public class Lecture extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long lectureId;
+
+    private LocalTime startTime;
+
+    private LocalTime endTime;
+
+    private String classRoom;
+
+    private String name;
+
+    private String lectureCode;
+
+    private String professor;
+
+    private Integer credit;
+
+    private List<WeekDay> lectureDays;
+
+
+}
