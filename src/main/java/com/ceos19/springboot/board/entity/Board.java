87b953489de0f66440f94f05c9a18ad42f274661
sunghyun1356 <sunghyun1356@naver.com>
2024-03-16 20:10:58 +0900

    [FEAT] Board Entity Create

--- src/main/java/com/ceos19/springboot/board/entity/Board.java
+++ src/main/java/com/ceos19/springboot/board/entity/Board.java
@@ -1,2 +1,20 @@
-package com.ceos19.springboot.board.entity;public class Board {
+package com.ceos19.springboot.board.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "board")
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long boardId;
+
+    @Column(name = "board_name", length = 20)
+    private String boardName;
+
+    @Column(name = "board_setting", length = 100)
+    private String boardSetting;
+
 }
