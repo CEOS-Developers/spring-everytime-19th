3b3f53fbcfc7254be1b1f3059ab482def9566746
kwon <kckc0608@naver.com>
2024-03-16 15:03:36 +0900

    feat: Category 엔티티에 롬복 적용

--- src/main/java/com/ceos19/springeverytime/domain/Category.java
+++ src/main/java/com/ceos19/springeverytime/domain/Category.java
@@ -1,24 +1,42 @@
 package com.ceos19.springeverytime.domain;
 
 import jakarta.persistence.*;
+import lombok.*;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@RequiredArgsConstructor
+@Getter
 public class Category {
     @Id
     @GeneratedValue
     private Long id;
 
+    @NonNull
     @Column(length = 100, nullable = false)
     private String name;
 
+    @NonNull
     @Column(length = 300, nullable = false)
     private String description;
 
+    @NonNull
     @Temporal(TemporalType.TIMESTAMP)
-    private Date createDate;
+    private Date createDate = new Date();
 
+    @NonNull
     @OneToOne
     private User manager;
+
+    @NonNull
+    @OneToMany(mappedBy = "category")
+    private List<Post> posts = new ArrayList<>();
+
+    public void addPost(Post post) {
+        posts.add(post);
+    }
 }
