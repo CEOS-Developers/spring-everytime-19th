ce00497bfc5ad0e893628644e744bb9d5b417f7c
kwon <kckc0608@naver.com>
2024-03-16 12:07:37 +0900

    feat: ChatMessage 엔티티 작성

---
+++ src/main/java/com/ceos19/springeverytime/domain/ChatMessage.java
@@ -0,0 +1,22 @@
+package com.ceos19.springeverytime.domain;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+public class ChatMessage {
+    @Id
+    private Long id;
+
+    @Column(length = 2000, nullable = false)
+    private String content;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date sendDate;
+
+    @ManyToOne
+    private ChatRoom room;
+
+    @ManyToOne
+    private User sender;
+}
