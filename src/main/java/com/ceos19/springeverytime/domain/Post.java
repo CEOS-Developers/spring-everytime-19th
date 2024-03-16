ac5d4ac1284344a60504ed2f7bb6d01eab568120
kwon <kckc0608@naver.com>
2024-03-16 15:04:21 +0900

    feat: 잘못된 연관관계 수정 후 롬복 적용

--- src/main/java/com/ceos19/springeverytime/domain/Post.java
+++ src/main/java/com/ceos19/springeverytime/domain/Post.java
@@ -1,34 +1,43 @@
 package com.ceos19.springeverytime.domain;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.Date;
 
 @Entity
+@RequiredArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Post {
     @Id
     @GeneratedValue
     private Long id;
 
+    @NonNull
     @Column(length = 100, nullable = false)
     private String title;
 
+    @NonNull
     @Column(length = 2000, nullable = false)
     private String content;
 
-    @Column(nullable = false)
+    @NonNull
     private boolean isAnonymous;
 
+    @NonNull
     @Temporal(TemporalType.TIMESTAMP)
     private Date createDate;
 
+    @NonNull
     @Temporal(TemporalType.TIMESTAMP)
     private Date modifyDate;
 
+    @NonNull
     @ManyToOne
     private User author;
 
-    @OneToOne
+    @NonNull
+    @ManyToOne
     private Category category;
 
 }
