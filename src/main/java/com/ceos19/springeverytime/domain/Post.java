df306f38234eb4b07474fb1b52a5d6c4d8b0ff27
kwon <kckc0608@naver.com>
2024-03-16 11:13:45 +0900

    feat: Post 엔티티 작성

---
+++ src/main/java/com/ceos19/springeverytime/domain/Post.java
@@ -0,0 +1,33 @@
+package com.ceos19.springeverytime.domain;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+public class Post {
+    @Id
+    private Long id;
+
+    @Column(length = 100, nullable = false)
+    private String title;
+
+    @Column(length = 2000, nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private boolean isAnonymous;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date modifyDate;
+
+    @ManyToOne
+    private User author;
+
+    @OneToOne
+    private Category category;
+
+}
