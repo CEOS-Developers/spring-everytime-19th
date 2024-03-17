16628b3436deb5c876fafa2bf6f06288394caaf6
yeong-hwan <jghff700@naver.com>
2024-03-18 01:27:13 +0900

    feat: implement friend entity

---
+++ src/main/java/com/ceos19/springboot/domain/Friend.java
@@ -0,0 +1,19 @@
+package com.ceos19.springboot.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "friend")
+public class Friend {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "friend_id", nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private Boolean isAccepted;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+}
