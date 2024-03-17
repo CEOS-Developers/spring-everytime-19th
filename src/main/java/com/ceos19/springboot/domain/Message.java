95c96cded5749fd2b71eeb4377f614a21e5a9868
yeong-hwan <jghff700@naver.com>
2024-03-18 01:25:35 +0900

    feat: implement message entity

---
+++ src/main/java/com/ceos19/springboot/domain/Message.java
@@ -0,0 +1,32 @@
+package com.ceos19.springboot.domain;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "message")
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "message_id", nullable = false)
+    private Long id;
+
+    @Column(nullable = true, length = 1000)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(columnDefinition = "TIMESTAMP", nullable = false)
+    private LocalDateTime transferTime;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sender_id")
+    private User sender;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "receiver_id")
+    private User receiver;
+}
