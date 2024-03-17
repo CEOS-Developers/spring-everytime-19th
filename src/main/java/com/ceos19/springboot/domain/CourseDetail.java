195f50ea8bc49f1b03af8f4024b03cf9c9294cc5
yeong-hwan <jghff700@naver.com>
2024-03-18 01:31:46 +0900

    feat: implement course_detail entity

---
+++ src/main/java/com/ceos19/springboot/domain/CourseDetail.java
@@ -0,0 +1,31 @@
+package com.ceos19.springboot.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "course_detail")
+public class CourseDetail {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "course_detail_id", nullable = false)
+    private Long id;
+
+    @Column(nullable = false, length = 20)
+    private String courseCode;
+
+    @Column(nullable = false, length = 20)
+    private String day;
+
+    @Column(nullable = false, length = 5)
+    private Integer startTime;
+
+    @Column(nullable = false, length = 5)
+    private Integer endTime;
+
+    @Column(nullable = false, length = 20)
+    private String classroom;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "course_id")
+    private Course course;
+}
