2ebb0a0d529b03e5d25e40a19d69d42b3602f28b
JEONG KI MIN <97235034+nimikgnoej@users.noreply.github.com>
2024-03-17 18:28:20 +0900

    Message 엔티티에 Builder 추가

--- src/main/java/com/ceos19/springboot/domain/Message.java
+++ src/main/java/com/ceos19/springboot/domain/Message.java
@@ -1,10 +1,18 @@
 package com.ceos19.springboot.domain;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Message {
     @Id
     @GeneratedValue
@@ -18,4 +26,13 @@ public class Message {
     @JoinColumn(name = "receiverId")
     private Users receiver;
 
+    private String content;
+
+    public Message(Users sender,
+                   Users receiver,
+                   String content) {
+        this.sender = sender;
+        this.receiver = receiver;
+        this.content = content;
+    }
 }
