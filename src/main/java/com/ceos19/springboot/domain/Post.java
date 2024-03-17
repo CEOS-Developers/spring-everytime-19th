c40cf8b966808af9915f50a4136e6e31b583ac83
yeong-hwan <jghff700@naver.com>
2024-03-18 01:17:25 +0900

    feat: implement post entity

---
+++ src/main/java/com/ceos19/springboot/domain/Post.java
@@ -0,0 +1,48 @@
+package com.ceos19.springboot.domain;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "post")
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_id", nullable = false)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Column(nullable = false, length = 65535)
+    private String content;
+
+    @Column(nullable = false, length = 10)
+    private Long like;
+
+    @Column(nullable = false, length = 10)
+    private Long report;
+
+    @Column(nullable = false, length = 10)
+    private Long scrap;
+
+    @Column(nullable = true, length = 20)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = true, length = 20)
+    private LocalDateTime editedAt;
+
+    @Column(nullable = false)
+    private boolean isAnonymous;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id")
+    private Board board;
+
+
+}
