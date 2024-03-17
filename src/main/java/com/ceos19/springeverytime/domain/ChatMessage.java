8653c5f13785353f983a8598cd6ef1067054e845
kwon <kckc0608@naver.com>
2024-03-17 23:01:28 +0900

    feat: ChatMessage - ChatRoom 연관관계 매핑 & ChatMessageRepository 작성

--- src/main/java/com/ceos19/springeverytime/domain/ChatMessage.java
+++ src/main/java/com/ceos19/springeverytime/domain/ChatMessage.java
@@ -1,24 +1,34 @@
 package com.ceos19.springeverytime.domain;
 
 import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 import java.util.Date;
 
+@Entity
+@NoArgsConstructor
+@RequiredArgsConstructor
 public class ChatMessage {
     @Id
     @GeneratedValue
     private Long messageId;
 
+    @NonNull
     @Column(length = 2000, nullable = false)
     private String content;
 
+    @NonNull
     @Temporal(TemporalType.TIMESTAMP)
     private Date sendDate;
 
+    @NonNull
     @ManyToOne
     @JoinColumn(name = "room_id")
     private ChatRoom room;
 
+    @NonNull
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User sender;
