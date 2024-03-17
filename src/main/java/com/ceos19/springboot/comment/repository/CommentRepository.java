3d1ebbabb759cf034135633d40278f57249ba34b
sunghyun1356 <sunghyun1356@naver.com>
2024-03-17 11:00:50 +0900

    [FEAT] Message Create

--- src/main/java/com/ceos19/springboot/comment/repository/CommentRepository.java
+++ src/main/java/com/ceos19/springboot/comment/repository/CommentRepository.java
@@ -3,5 +3,5 @@ package com.ceos19.springboot.comment.repository;
 import com.ceos19.springboot.comment.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CommentLikeRepository extends JpaRepository<Comment,Long> {
+public interface CommentRepository extends JpaRepository<Comment,Long> {
 }
---
+++ src/main/java/com/ceos19/springboot/message/entity/Message.java
@@ -0,0 +1,36 @@
+package com.ceos19.springboot.message.entity;
+
+import com.ceos19.springboot.user.entity.User;
+import jakarta.persistence.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@Entity
+@Table(name = "message")
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private boolean deletedBySender;
+
+    @Column(nullable = false)
+    private boolean deletedByReceiver;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sender_id")
+    @OnDelete(action = OnDeleteAction.NO_ACTION)
+    private User sender;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "receiver_id")
+    @OnDelete(action = OnDeleteAction.NO_ACTION)
+    private User receiver;
+}
