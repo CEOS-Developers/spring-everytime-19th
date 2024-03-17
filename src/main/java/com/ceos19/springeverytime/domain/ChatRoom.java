42beece10a8e184ca548f53549a0cd6e1659a5b3
kwon <kckc0608@naver.com>
2024-03-17 22:49:33 +0900

    feat: ChatRoom 연관관계 매핑 & ChatRoomRepository 생성

--- src/main/java/com/ceos19/springeverytime/domain/ChatRoom.java
+++ src/main/java/com/ceos19/springeverytime/domain/ChatRoom.java
@@ -1,17 +1,26 @@
 package com.ceos19.springeverytime.domain;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@RequiredArgsConstructor
 public class ChatRoom {
     @Id
     @GeneratedValue
     private Long roomId;
 
+    @NonNull
     @ManyToOne
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "user1_id")
     private User member1;
 
+    @NonNull
     @ManyToOne
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "user2_id")
     private User member2;
 }
---
+++ src/main/java/com/ceos19/springeverytime/repository/ChatRoomRepository.java
@@ -0,0 +1,29 @@
+package com.ceos19.springeverytime.repository;
+
+import com.ceos19.springeverytime.domain.ChatRoom;
+import com.ceos19.springeverytime.domain.User;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ChatRoomRepository {
+    private final EntityManager em;
+
+    public void save(ChatRoom room) {
+        em.persist(room);
+    }
+
+    public ChatRoom findOne(Long roomId) {
+        return em.find(ChatRoom.class, roomId);
+    }
+
+    public List<ChatRoom> findAllByUser(User user) {
+        return em.createQuery("select r from ChatRoom r where r.member1 = :user or r.member2 = :user", ChatRoom.class)
+                .setParameter("user", user)
+                .getResultList();
+    }
+}
