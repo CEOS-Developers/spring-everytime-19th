596e31c9b1266ff1af6f8e8d97cca31614066ed4
yeong-hwan <jghff700@naver.com>
2024-03-18 02:02:16 +0900

    feat: implement repository interfaces

---
+++ src/main/java/com/ceos19/springboot/repository/UserRepository.java
@@ -0,0 +1,19 @@
+package com.ceos19.springboot.repository;
+
+import com.ceos19.springboot.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    List<User> findAll();
+
+    Optional<User> findById(@Param("userId") long userId);
+
+    Optional<User> findByName(@Param("name")String name);
+
+    Optional<User> findByUsername(@Param("username")String username);
+}
