418f2ba188ed8c28a3a8c4fe0d33f21c506bcf26
kwon <kckc0608@naver.com>
2024-03-17 22:38:36 +0900

    feat: Comment - Post 연관 관계 매핑 & 테스트 작성

--- src/main/java/com/ceos19/springeverytime/domain/Comment.java
+++ src/main/java/com/ceos19/springeverytime/domain/Comment.java
@@ -1,18 +1,27 @@
 package com.ceos19.springeverytime.domain;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@RequiredArgsConstructor
 public class Comment {
     @Id@GeneratedValue
     private Long commentId;
 
+    @NonNull
     @Column(length = 2000, nullable = false)
     private String content;
 
+    @NonNull
     @Column(nullable = false)
     private boolean isAnonymous;
 
+    @NonNull
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User author;
@@ -21,6 +30,7 @@ public class Comment {
     @JoinColumn(name = "parent_comment_id")
     private Comment parentComment;
 
+    @NonNull
     @ManyToOne
     @JoinColumn(name = "post_id")
     private Post post;
