d7c91c0a80e8c4a15cc25b3e8b3d3942612a8f8d
hyeongjun im <limbs713@naver.com>
2024-03-17 16:46:21 +0900

    :sparkles: 추가 도메인 엔티티
    Room과 ReadStatus를 추가했습니다

---
+++ src/main/java/com/ceos19/springeverytime/domain/Room.java
@@ -0,0 +1,30 @@
+package com.ceos19.springeverytime.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+public class Room {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "room_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User participant1;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User participant2;
+
+}
