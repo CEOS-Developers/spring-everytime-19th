7a2cc60ee6c3439de4474549d946ed6e36116fc7
leeedohyun <ldh19738@gmail.com>
2024-03-20 16:29:47 +0900

    test: 게시글 생성 서비스 레이어 테스트

--- src/test/java/com/ceos19/everytime/service/PostServiceTest.java
+++ src/test/java/com/ceos19/everytime/service/PostServiceTest.java
@@ -3,6 +3,7 @@ package com.ceos19.everytime.service;
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
 import static org.mockito.BDDMockito.anyLong;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 
 import java.util.Optional;
 
@@ -15,9 +16,12 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import com.ceos19.everytime.domain.Board;
 import com.ceos19.everytime.domain.Post;
 import com.ceos19.everytime.domain.User;
+import com.ceos19.everytime.dto.request.PostCreateRequestDto;
 import com.ceos19.everytime.dto.request.PostRequestDto;
 import com.ceos19.everytime.dto.response.PostResponseDto;
+import com.ceos19.everytime.repository.BoardRepository;
 import com.ceos19.everytime.repository.PostRepository;
+import com.ceos19.everytime.repository.UserRepository;
 
 @ExtendWith(MockitoExtension.class)
 class PostServiceTest {
@@ -25,8 +29,51 @@ class PostServiceTest {
     @Mock
     private PostRepository postRepository;
 
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private BoardRepository boardRepository;
+
     @InjectMocks
     private PostService postService;
+
+    @Test
+    void 게시글_생성_테스트() {
+        // given
+        final PostCreateRequestDto request = PostCreateRequestDto.builder()
+                .boardId(1L)
+                .title("title")
+                .content("안녕하세요~")
+                .isAnonymous(true)
+                .userId(1L)
+                .build();
+
+        final User user = User.builder()
+                .nickname("hello")
+                .build();
+        final Board board = Board.builder()
+                .name("boardName")
+                .build();
+        final Post post = Post.builder()
+                .title("title")
+                .writer(user)
+                .content("안녕하세요~")
+                .board(board)
+                .isAnonymous(true)
+                .build();
+
+        given(userRepository.findById(anyLong()))
+                .willReturn(Optional.of(user));
+        given(boardRepository.findById(anyLong()))
+                .willReturn(Optional.of(board));
+
+        // when & then
+        postService.createPost(request);
+
+        // then
+        verify(postRepository).save(post);
+    }
 
     @Test
     void 게시글을_조회한다() {
@@ -39,9 +86,8 @@ class PostServiceTest {
                 .name("boardName")
                 .build();
         final Post post = Post.builder()
-                .id(1L)
                 .title("title")
-                .user(user)
+                .writer(user)
                 .content("안녕하세요~")
                 .board(board)
                 .isAnonymous(true)
