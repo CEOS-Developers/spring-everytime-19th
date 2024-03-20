726a3435998416dfa1a2a2b04c18eefb270c3bbc
kwon <kckc0608@naver.com>
2024-03-21 01:48:29 +0900

    test: Category Service 테스트 작성

---
+++ src/test/java/com/ceos19/springeverytime/service/CategoryServiceTest.java
@@ -0,0 +1,80 @@
+package com.ceos19.springeverytime.service;
+
+import com.ceos19.springeverytime.domain.Category;
+import com.ceos19.springeverytime.domain.User;
+import com.ceos19.springeverytime.repository.CategoryRepository;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+public class CategoryServiceTest {
+    @Mock
+    CategoryRepository categoryRepository;
+
+    @InjectMocks
+    CategoryService categoryService;
+
+    User user1, user2;
+
+    @BeforeEach
+    void 테스트_셋업() {
+        user1 = new User(
+            "test",
+            "adsfbsa234@ad",
+            "nicnname",
+            "kim",
+            "computer",
+            "20",
+            "test@exmaple.com",
+            true
+        );
+
+        user2 = new User(
+                "test2",
+                "adsfbsa234@ad",
+                "nickname2",
+                "kwon",
+                "data",
+                "21",
+                "test2@exmaple.com",
+                true
+        );
+    }
+
+    @Test
+    @DisplayName("게시판 생성 테스트")
+    void 게시판_생성_테스트() {
+        // given
+        Category category = new Category("자유게시판", "", user1);
+        given(categoryRepository.save(any(Category.class))).willReturn(category.getCategoryId());
+
+        // when
+        Long newCategoryId = categoryService.create(category);
+
+        // then
+        assertThat(newCategoryId).isEqualTo(category.getCategoryId());
+    }
+
+    @Test
+    @DisplayName("게시판 관리자 변경 테스트")
+    void 게시판_관리자_변경_테스트() {
+        // given
+        Category category = new Category("자유게시판", "", user1);
+        given(categoryRepository.findOne(any())).willReturn(category);
+
+        // when
+        Category test_category = categoryService.findOne(category.getCategoryId());
+        test_category.changeManager(user2);
+
+        // then
+        assertThat(categoryService.findOne(category.getCategoryId()).getManager()).isEqualTo(user2);
+    }
+}
