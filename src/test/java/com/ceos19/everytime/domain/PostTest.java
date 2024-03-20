a0c340935e0bb837d181528ea4446a0476004a9a
leeedohyun <ldh19738@gmail.com>
2024-03-20 14:57:05 +0900

    test: isAnonymous의 여부에 따라 작성자의 닉네임 설정 테스트

---
+++ src/test/java/com/ceos19/everytime/domain/PostTest.java
@@ -0,0 +1,36 @@
+package com.ceos19.everytime.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import com.ceos19.everytime.domain.Board;
+import com.ceos19.everytime.domain.Post;
+import com.ceos19.everytime.domain.User;
+
+class PostTest {
+
+    @ParameterizedTest
+    @CsvSource(value = {"true, 익명", "false, nickname"})
+    void isAnounymous가_true이면_작성자의_닉네임은_익명이_된다(boolean isAnonymous, String expected) {
+        // given
+        final User user = User.builder()
+                .nickname("nickname")
+                .build();
+        final Board board = Board.builder()
+                .name("boardName")
+                .build();
+        final Post post = Post.builder()
+                .user(user)
+                .board(board)
+                .isAnonymous(isAnonymous)
+                .build();
+
+        // when
+        final String nickname = post.getWriterNickname();
+
+        // then
+        assertThat(nickname).isEqualTo(expected);
+    }
+}
