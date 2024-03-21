d5fddcc7461f7b1cab093eadbe5c25d03890772c
kwon <kckc0608@naver.com>
2024-03-21 16:13:06 +0900

    feat: Post Service 작성

---
+++ src/main/java/com/ceos19/springeverytime/service/PostService.java
@@ -0,0 +1,22 @@
+package com.ceos19.springeverytime.service;
+
+import com.ceos19.springeverytime.domain.Post;
+import com.ceos19.springeverytime.repository.PostRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class PostService {
+    private final PostRepository postRepository;
+
+    public Post findOne(Long postId) {
+        return postRepository.findOne(postId);
+    }
+
+    public void save(Post post) {
+        postRepository.save(post);
+    }
+}
