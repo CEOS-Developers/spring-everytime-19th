681b67d7acf8bf4889521d45765005f3347335bd
leeedohyun <ldh19738@gmail.com>
2024-03-21 09:49:14 +0900

    test: 게시판으로 모든 게시글 조회 테스트

--- src/test/java/com/ceos19/everytime/repository/PostRepositoryTest.java
+++ src/test/java/com/ceos19/everytime/repository/PostRepositoryTest.java
@@ -1,7 +1,10 @@
 package com.ceos19.everytime.repository;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
+
+import java.util.List;
+
+import jakarta.persistence.EntityManager;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,6 +14,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 
 import com.ceos19.everytime.config.JpaAuditingConfig;
+import com.ceos19.everytime.domain.Board;
 import com.ceos19.everytime.domain.Post;
 import com.ceos19.everytime.domain.User;
 
@@ -25,7 +29,14 @@ class PostRepositoryTest {
     @Autowired
     private PostRepository postRepository;
 
+    @Autowired
+    private BoardRepository boardRepository;
+
+    @Autowired
+    private EntityManager em;
+
     private User user;
+    private Board board;
     private Post post1;
     private Post post2;
     private Post post3;
@@ -37,21 +48,28 @@ class PostRepositoryTest {
                 .password("qwe123!")
                 .nickname("nick")
                 .build();
+        board = Board.builder()
+                .name("board")
+                .build();
         post1 = Post.builder()
                 .title("title1")
                 .writer(user)
+                .board(board)
                 .content("content1")
                 .build();
         post2 = Post.builder()
-                .title("title1")
+                .title("title2")
                 .writer(user)
-                .content("content1")
+                .board(board)
+                .content("content2")
                 .build();
         post3 = Post.builder()
-                .title("title1")
+                .title("title3")
                 .writer(user)
-                .content("content1")
+                .board(board)
+                .content("content3")
                 .build();
+        boardRepository.save(board);
         userRepository.save(user);
         postRepository.save(post1);
         postRepository.save(post2);
@@ -66,10 +84,24 @@ class PostRepositoryTest {
         final Post post3 = postRepository.findById(3L).get();
 
         // then
-        assertAll(
-                () -> assertThat(post1).usingRecursiveComparison().isEqualTo(this.post1),
-                () -> assertThat(post2).usingRecursiveComparison().isEqualTo(this.post2),
-                () -> assertThat(post3).usingRecursiveComparison().isEqualTo(this.post3)
-        );
+        assertSoftly(softly -> {
+            softly.assertThat(post1).isEqualTo(this.post1);
+            softly.assertThat(post2).isEqualTo(this.post2);
+            softly.assertThat(post3).isEqualTo(this.post3);
+        });
+    }
+
+    @Test
+    void 게시판에_해당하는_모든_게시글을_조회한다() {
+        // given & when
+        final List<Post> posts = postRepository.findAllFetchJoin(board);
+
+        // then
+        assertSoftly(softly -> {
+            softly.assertThat(posts.size()).isEqualTo(3);
+            softly.assertThat(posts.get(0)).isEqualTo(post1);
+            softly.assertThat(posts.get(1)).isEqualTo(post2);
+            softly.assertThat(posts.get(2)).isEqualTo(post3);
+        });
     }
 }
