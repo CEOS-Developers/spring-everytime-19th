e7eaa27804093d5ae046e7e596f02fa7a267f34b
parking0 <p980710@gmail.com>
2024-03-21 19:16:23 +0900

    feat : MemberService 구현

--- src/main/java/com/ceos19/everytime/domain/Member.java
+++ src/main/java/com/ceos19/everytime/domain/Member.java
@@ -11,19 +11,22 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
 
+    public static final int MIN_USERNAME_LENGTH = 1;
+    public static final int MAX_USERNAME_LENGTH = 10;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "member_id")
     private Long id;
 
-    @Column(nullable = false, length = 20)
+    @Column(nullable = false, unique = true, length = 20)
     private String loginId;
 
     @Column(nullable = false, length = 30)
     private String userPw;
 
     @Column(nullable = false, unique = true, length = 10)
-    private String userName;
+    private String username;
 
     @Column(nullable = false)
     private String email;
@@ -33,12 +36,28 @@ public class Member {
     private University university;
 
     @Builder
-    public Member(final String userName, final String loginId, final String userPw, final String email, final University university) {
-        this.userName = userName;
+    public Member(final String username, final String loginId, final String userPw, final String email, final University university) {
+        this.username = username;
         this.loginId = loginId;
         this.userPw = userPw;
         this.email = email;
         this.university = university;
     }
+
+    public boolean changeUsername(final String username) {      //반환을 어떻게 해아할지?
+        if(validateUsername(username) != null){
+            this.username = username;
+            return true;
+        }
+        return false;
+    }
+
+    private String validateUsername(final String username) {
+        if (username.isEmpty() ||  username.length() > MAX_USERNAME_LENGTH) {
+            return null;
+        }
+        return username;
+    }
+
 
 }
