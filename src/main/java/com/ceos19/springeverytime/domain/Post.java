6106da241f18a3bad831a1f0d5d1f46c9d7cd347
kwon <kckc0608@naver.com>
2024-03-24 15:05:49 +0900

    feat: 댓글 작성 연관 메서드 추가

--- src/main/java/com/ceos19/springeverytime/domain/Post.java
+++ src/main/java/com/ceos19/springeverytime/domain/Post.java
@@ -39,18 +39,18 @@ public class Post {
     private Date modifyDate;
 
     @NonNull
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     private User author;
 
     @NonNull
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "category_id")
     private Category category;
 
     @OneToMany(mappedBy = "post", cascade = CascadeType.REMOVE)
     private List<PostLike> postLikes = new ArrayList<>();
 
-    @OneToMany(mappedBy = "post", cascade = CascadeType.REMOVE)
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL)
     private List<Comment> comments = new ArrayList<>();
 
     @OneToMany(mappedBy = "post", cascade = CascadeType.ALL)
@@ -62,5 +62,11 @@ public class Post {
     public void modify(String title, String content) {
         this.title = title;
         this.content = content;
+    }
+
+    public Comment addComment(User author, String content, boolean isAnonymous) {
+        Comment comment = Comment.builder().post(this).author(author).content(content).isAnonymous(isAnonymous).build();
+        this.comments.add(comment);
+        return comment;
     }
 }
