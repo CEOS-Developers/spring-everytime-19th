039f8fd5c8a0396e4977c0be879753f6cb3e29c9
kwon <kckc0608@naver.com>
2024-03-24 15:06:46 +0900

    feat: Comment 엔티티에 빌더 패턴 및 fetch 설정 추가

--- src/main/java/com/ceos19/springeverytime/domain/Comment.java
+++ src/main/java/com/ceos19/springeverytime/domain/Comment.java
@@ -12,25 +12,34 @@ public class Comment {
     private Long commentId;
 
     @NonNull
-    @Column(length = 2000, nullable = false)
+    @Column(length = 2000, nullable = false, updatable = false)
     private String content;
 
     @NonNull
-    @Column(nullable = false)
+    @Column(nullable = false, updatable = false)
     private boolean isAnonymous;
 
     @NonNull
-    @ManyToOne
-    @JoinColumn(name = "user_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", updatable = false)
     private User author;
 
-    @ManyToOne
-    @JoinColumn(name = "parent_comment_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_comment_id", updatable = false)
     @Setter
     private Comment parentComment;
 
     @NonNull
-    @ManyToOne
-    @JoinColumn(name = "post_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", updatable = false)
     private Post post;
+
+    @Builder
+    public Comment(@NonNull String content, @NonNull boolean isAnonymous, @NonNull User author, Comment parentComment, @NonNull Post post) {
+        this.content = content;
+        this.isAnonymous = isAnonymous;
+        this.author = author;
+        this.parentComment = parentComment;
+        this.post = post;
+    }
 }
