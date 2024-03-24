0e3408d27eff84ccf681b9520cfce7184406b49b
kwon <kckc0608@naver.com>
2024-03-24 12:49:51 +0900

    refactor: 엔티티 객체 생성 코드 분리

---
+++ src/test/java/com/ceos19/springeverytime/common/EntityGenerator.java
@@ -0,0 +1,34 @@
+package com.ceos19.springeverytime.common;
+
+import com.ceos19.springeverytime.domain.Category;
+import com.ceos19.springeverytime.domain.Comment;
+import com.ceos19.springeverytime.domain.Post;
+import com.ceos19.springeverytime.domain.User;
+
+import java.util.Date;
+
+public class EntityGenerator {
+    public static User generateUser(String loginId) {
+        return new User(
+                loginId,
+            "1234",
+            "nickname",
+            "kim",
+            "computer",
+            "20",
+            "test@example.com"
+        );
+    }
+
+    public static Category generateCategory(User manager) {
+        return new Category("자유게시판", "자유롭게 이야기 해봐요", manager);
+    }
+
+    public static Post generatePost(User author, Category category) {
+        return new Post("첫번째 글", "첫번째 글입니다.", true, new Date(), new Date(), author, category);
+    }
+
+    public static Comment generateComment(User author, Post post) {
+        return new Comment("content", true, author, post);
+    }
+}
