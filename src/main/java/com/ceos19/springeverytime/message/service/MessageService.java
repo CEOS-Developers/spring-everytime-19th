ec60433bf21af862bf55ce6edf909c25582b1373
hyeongjun im <limbs713@naver.com>
2024-03-25 10:55:29 +0900

    :sparkles: 추가 MessageService에 조회 메소드

--- src/main/java/com/ceos19/springeverytime/message/service/MessageService.java
+++ src/main/java/com/ceos19/springeverytime/message/service/MessageService.java
@@ -1,5 +1,6 @@
 package com.ceos19.springeverytime.message.service;
 
+import com.ceos19.springeverytime.message.domain.Message;
 import com.ceos19.springeverytime.room.domain.Room;
 import com.ceos19.springeverytime.message.dto.MessageDto;
 import com.ceos19.springeverytime.message.repository.MessageRepository;
@@ -7,6 +8,7 @@ import com.ceos19.springeverytime.room.repository.RoomRepository;
 import com.ceos19.springeverytime.user.domain.User;
 import com.ceos19.springeverytime.user.repository.UserRepository;
 import jakarta.persistence.EntityNotFoundException;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -26,5 +28,17 @@ public class MessageService {
                 .orElseThrow(EntityNotFoundException::new);
 
         messageRepository.save(messageDto.toEntity(room, user));
+    }
+
+    public void deleteMessage(Long messageId){
+        messageRepository.deleteById(messageId);
+    }
+
+    public List<Message> getMessagesByRoom(Long roomId){
+        return messageRepository.findMessagesByRoom_Id(roomId);
+    }
+
+    public List<Message> getMessagesByUser(Long userId){
+        return messageRepository.findMessagesBySender_Id(userId);
     }
 }
