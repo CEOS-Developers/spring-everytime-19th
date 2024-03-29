3e6846699c0139320cdbd5c7c8d9ff589ede63be
leeedohyun <ldh19738@gmail.com>
2024-03-29 17:45:41 +0900

    test: 모든 게시글 조회 테스트

--- src/test/java/com/ceos19/everytime/post/controller/PostControllerTest.java
+++ src/test/java/com/ceos19/everytime/post/controller/PostControllerTest.java
@@ -1,9 +1,15 @@
 package com.ceos19.everytime.post.controller;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.doNothing;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,12 +18,16 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.ceos19.everytime.board.dto.request.BoardPostsRequestDto;
+import com.ceos19.everytime.board.dto.response.BoardPostsResponseDto;
 import com.ceos19.everytime.post.dto.request.PostCreateRequestDto;
 import com.ceos19.everytime.post.service.PostService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @WebMvcTest(PostController.class)
 class PostControllerTest {
+
+    private static final String DEFAULT_POST_URL = "/api/post";
 
     @Autowired
     private MockMvc mockMvc;
@@ -36,9 +46,33 @@ class PostControllerTest {
         doNothing().when(postService).createPost(any());
 
         // when & then
-        mockMvc.perform(post("/api/post")
+        mockMvc.perform(post(DEFAULT_POST_URL)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(request)))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @Test
+    void 게시판에_해당하는_모든_게시글을_조회한다() throws Exception {
+        // given
+        final BoardPostsRequestDto request = new BoardPostsRequestDto(1L);
+
+        given(postService.getPosts(any()))
+                .willReturn(List.of(new BoardPostsResponseDto("title1", "writerNickname1"),
+                        new BoardPostsResponseDto("title2", "writerNickname2")));
+
+        // when & then
+        mockMvc.perform(get(DEFAULT_POST_URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpectAll(
+                        jsonPath("$[0].title").value("title1"),
+                        jsonPath("$[0].writerNickname").value("writerNickname1"),
+                        jsonPath("$[1].title").value("title2"),
+                        jsonPath("$[1].writerNickname").value("writerNickname2")
+                );
     }
 }
