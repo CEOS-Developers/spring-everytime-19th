b7c4a7e3c34ae53688cf7ff3e8ea198a7ed90f6b
JEONG KI MIN <97235034+nimikgnoej@users.noreply.github.com>
2024-03-30 01:00:45 +0900

    [feat] API Response 설정

---
+++ src/main/java/com/ceos19/springboot/exception/ApiResponse.java
@@ -0,0 +1,28 @@
+package com.ceos19.springboot.exception;
+
+import lombok.Getter;
+import lombok.Builder;
+
+@Getter
+public class ApiResponse<T> {
+
+    // API 응답 결과 Response
+    private T result;
+
+    // API 응답 코드 Response
+    private int resultCode;
+
+    // API 응답 코드 Message
+    private String resultMsg;
+
+    @Builder
+    public ApiResponse(final T result, final int resultCode, final String resultMsg) {
+        this.result = result;
+        this.resultCode = resultCode;
+        this.resultMsg = resultMsg;
+    }
+    public static <T> ApiResponse<T> of(int resultCode, String resultMsg, T result) {
+        return new ApiResponse<>(result,resultCode,resultMsg);
+    }
+}
+
