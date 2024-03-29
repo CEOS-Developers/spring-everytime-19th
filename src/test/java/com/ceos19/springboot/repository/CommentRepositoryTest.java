3083c3d1a23b2eb3a13a76be9aada3a0c3156b73
JEONG KI MIN <97235034+nimikgnoej@users.noreply.github.com>
2024-03-30 01:06:52 +0900

    [feat] 테스트 코드 변경

---
+++ src/test/java/com/ceos19/springboot/repository/CommentRepositoryTest.java
@@ -0,0 +1,29 @@
+package com.ceos19.springboot.repository;
+
+import com.ceos19.springboot.comment.domain.Comment;
+import com.ceos19.springboot.comment.repository.CommentRepository;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Transactional
+public class CommentRepositoryTest {
+    @Autowired
+    private CommentRepository commentRepository;
+
+    @Test
+    public void commentSaveTest() throws Exception {
+        //given
+        Comment comment = Comment.builder().build();
+        //when
+        commentRepository.save(comment);
+        Comment findComment = commentRepository.findById(comment.getCommentId()).orElseThrow(() -> new EntityNotFoundException("없음"));
+        //then
+        Assertions.assertThat(comment.getCommentId()).isEqualTo(findComment.getCommentId());
+
+    }
+}
---
+++ src/test/java/com/ceos19/springboot/repository/MessageRepositoryTest.java
@@ -0,0 +1,29 @@
+package com.ceos19.springboot.repository;
+
+import com.ceos19.springboot.comment.domain.Comment;
+import com.ceos19.springboot.comment.repository.CommentRepository;
+import com.ceos19.springboot.message.domain.Message;
+import com.ceos19.springboot.message.repository.MessageRepository;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Transactional
+public class MessageRepositoryTest {
+    @Autowired
+    private MessageRepository messageRepository;
+    @Test
+    public void commentSaveTest() throws Exception {
+        //given
+        Message message = Message.builder().build();
+        //when
+        messageRepository.save(message);
+        Message findMessage = messageRepository.findById(message.getMessageId()).orElseThrow(() -> new EntityNotFoundException("없음"));
+        //then
+        Assertions.assertThat(message.getMessageId()).isEqualTo(findMessage.getMessageId());
+    }
+}
