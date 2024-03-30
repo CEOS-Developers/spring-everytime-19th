0a8148952cad2f7cbde128f6a2b038a56ef2ba8d
Min Jeon <mulsoap0504@gmail.com>
2024-03-30 21:55:49 +0900

    feat: 정적팩토리 메서드 리팩토링

--- src/main/java/com/ceos19/everytime/dto/ReadSchoolResponse.java
+++ src/main/java/com/ceos19/everytime/dto/ReadSchoolResponse.java
@@ -1,17 +1,16 @@
 package com.ceos19.everytime.dto;
 
 import com.ceos19.everytime.domain.School;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class ReadSchoolResponse {
     private Long id;
     private String name;
 
     public static ReadSchoolResponse from(School school) {
-        ReadSchoolResponse readSchoolResponse = new ReadSchoolResponse();
-        readSchoolResponse.id = school.getId();
-        readSchoolResponse.name = school.getName();
-        return readSchoolResponse;
+        return new ReadSchoolResponse(school.getId(), school.getName());
     }
 }
