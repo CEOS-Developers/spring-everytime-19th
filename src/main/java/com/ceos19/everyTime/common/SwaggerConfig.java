22b7019b57ea2cfb05fb834d9892db7549563e87
Jinu Yi <dionisos198@naver.com>
2024-03-30 20:55:00 +0900

    Feat: swagger 세팅

---
+++ src/main/java/com/ceos19/everyTime/common/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package com.ceos19.everyTime.common;
+
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        String jwt = "JWT";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwt);
+        Components components = new Components().addSecuritySchemes(jwt, new SecurityScheme()
+            .name(jwt)
+            .type(SecurityScheme.Type.HTTP)
+            .scheme("bearer")
+            .bearerFormat("JWT")
+        );
+        return new OpenAPI()
+            .components(new Components())
+            .info(apiInfo())
+            .addSecurityItem(securityRequirement)
+            .components(components);
+    }
+    private Info apiInfo() {
+        return new Info()
+            .title("에타") // API의 제목
+            .description("에타 테스트~") // API에 대한 설명
+            .version("jinu-ver"); // API의 버전
+    }
+}
