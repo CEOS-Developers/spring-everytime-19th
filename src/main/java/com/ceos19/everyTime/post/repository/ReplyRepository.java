856d1cf7084aa04db8ec6251fe7d1cdf0c91a905
Jinu Yi <dionisos198@naver.com>
2024-03-30 21:04:35 +0900

    Refactor: soft delete 적용

--- src/main/java/com/ceos19/everyTime/post/repository/ReplyRepository.java
+++ src/main/java/com/ceos19/everyTime/post/repository/ReplyRepository.java
@@ -16,15 +16,18 @@ public interface ReplyRepository extends JpaRepository<Reply,Long> {
     @Query("select r from Reply r where r.post.id=:postId and r.parent=null ")
     List<Reply> findParentReplyByPostId(@Param("postId") Long postId);
 
-    @Query("select distinct r from Reply r  left join fetch r.childList join fetch r.member where r.post.id=:postId and r.parent=null")
+    @Query("select distinct r from Reply r  left join fetch r.childList join fetch r.member where r.post.id=:postId and r.parent=null and (r.deleted=false or size(r.childList) > 0) order by r.createdAt")
     List<Reply> findParentReplyByPostIdWithFetchMember(@Param("postId")Long postId);
 
-    @Query("select r from Reply r  where r.parent.id=:parentId")
+    @Query("select r from Reply r  where r.parent.id=:parentId and r.deleted = false order by r.createdAt")
     List<Reply> findChildByParentId(@Param("parentId") Long parentId);
 
     @Query("select r from Reply r where r.member.id=:memberId and r.post.id=:postId and r.isHideNickName=true order by r.id limit 1")
     Optional<Reply> findDefaultNickNameReplyByMemberIdAndPostIdLimitOne(@Param("memberId")Long memberId,@Param("postId") Long postId);
 
     boolean existsByParentId(Long parentId);
+
+    @Query("select r from Reply r where r.id=:replyId and r.deleted=false")
+    Optional<Reply> findNotDeletedReply(@Param("replyId") Long replyId);
 
 }
