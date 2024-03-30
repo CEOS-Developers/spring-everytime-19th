dfc1784bc4d36d1685129d17e94edfb426fa9f99
hyeongjun im <limbs713@naver.com>
2024-03-30 22:17:42 +0900

    :recycle: 메소드명 수정

--- src/main/java/com/ceos19/springeverytime/user/service/UserService.java
+++ src/main/java/com/ceos19/springeverytime/user/service/UserService.java
@@ -1,9 +1,11 @@
 package com.ceos19.springeverytime.user.service;
 
 import com.ceos19.springeverytime.user.domain.User;
-import com.ceos19.springeverytime.user.dto.request.UserJoinDto;
+import com.ceos19.springeverytime.user.dto.request.UserRequestDto;
 import com.ceos19.springeverytime.user.repository.UserRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,20 +13,33 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class UserService {
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
     @Transactional
-    public void join(UserJoinDto userJoinDto) {
-        if (userRepository.existsUserByLoginId(userJoinDto.getId())) {
+    public void createUser(UserRequestDto userRequestDto) {
+        if (userRepository.existsUserByLoginId(userRequestDto.getId())) {
             throw new IllegalStateException("User already exists");
         }
 
-        User user = userJoinDto.toEntity();
+        User user = userRequestDto.toEntity();
         userRepository.save(user);
     }
 
-    public User getUser(Long userId) {
+    public User readUser(Long userId) {
         return userRepository.findUserById(userId)
                 .orElseThrow(IllegalStateException::new);
+    }
+
+    public List<User> readAllUsers(){
+        return userRepository.findAll();
+    }
+
+    public void deleteUser(Long userId) {
+        userRepository.deleteById(userId);
+    }
+
+    public void updateUser(UserRequestDto userRequestDto, Long userId) throws Exception{
+        User user = userRepository.findUserById(userId).orElseThrow(NotFoundException::new);
+        user.update(userRequestDto.getPassword());
     }
 }
