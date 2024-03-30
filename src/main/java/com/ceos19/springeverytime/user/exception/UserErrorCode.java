18f142aba550a5a988c2ee5cb186cbaa2ad6d450
hyeongjun im <limbs713@naver.com>
2024-03-30 17:53:08 +0900

    :sparkles: 추가 user exception 기능

---
+++ src/main/java/com/ceos19/springeverytime/user/exception/UserErrorCode.java
@@ -0,0 +1,20 @@
+package com.ceos19.springeverytime.user.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum UserErrorCode{
+    USER_ALREADY_EXIST(HttpStatus.CONFLICT, "이미 존재하는 회원 정보입니다."),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 회원입니다."),
+    INVALID_USER_INFO(HttpStatus.BAD_REQUEST, "잘못된 회원 정보입니다.");
+
+
+    private final HttpStatus httpStatus;
+    private final String message;
+
+    UserErrorCode(HttpStatus httpStatus, String message) {
+        this.httpStatus = httpStatus;
+        this.message = message;
+    }
+}
