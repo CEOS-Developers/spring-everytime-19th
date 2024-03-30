7cec872e33ce43dad98e062e748ba24b1d4cd375
hyeongjun im <limbs713@naver.com>
2024-03-30 22:20:06 +0900

    :recycle: findByUserId 메소드명 변경

--- src/main/java/com/ceos19/springeverytime/postlike/repository/PostLikeRepository.java
+++ src/main/java/com/ceos19/springeverytime/postlike/repository/PostLikeRepository.java
@@ -2,11 +2,13 @@ package com.ceos19.springeverytime.postlike.repository;
 
 import com.ceos19.springeverytime.postlike.domain.PostLike;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostLikeRepository extends JpaRepository<PostLike,Long> {
 
     List<PostLike> findByPostId(Long id);
 
-    List<PostLike> findByUserId(Long id);
+    Optional<PostLike> findByUser_IdAndPost_Id(Long userId, Long postId);
+
 }
