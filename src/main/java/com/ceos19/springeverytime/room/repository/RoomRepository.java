3a56659bd85cbfd4a209b9ea4c1e38efe7692591
hyeongjun im <limbs713@naver.com>
2024-03-30 22:16:17 +0900

    :recycle: findByParticipant1_IdOrParticipant2_Id 메소드 수정

--- src/main/java/com/ceos19/springeverytime/room/repository/RoomRepository.java
+++ src/main/java/com/ceos19/springeverytime/room/repository/RoomRepository.java
@@ -3,8 +3,12 @@ package com.ceos19.springeverytime.room.repository;
 import com.ceos19.springeverytime.room.domain.Room;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface RoomRepository extends JpaRepository<Room , Long> {
 
-    List<Room> findByParticipant1_IdOrParticipant2_Id(Long userId);
+    @Query("select r "
+            + "from Room r "
+            + "where r.participant1.id = :id or r.participant2.id = : id")
+    List<Room> findByParticipant1_IdOrParticipant2_Id(Long id);
 }
