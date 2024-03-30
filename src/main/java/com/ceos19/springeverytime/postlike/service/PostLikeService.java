9e05a9404306b0a00b31c640903d3ba2bff9d9bf
hyeongjun im <limbs713@naver.com>
2024-03-30 22:29:27 +0900

    :recycle: PostLikeService에서 service 의존성 제거

--- src/main/java/com/ceos19/springeverytime/postlike/service/PostLikeService.java
+++ src/main/java/com/ceos19/springeverytime/postlike/service/PostLikeService.java
@@ -1,14 +1,17 @@
 package com.ceos19.springeverytime.postlike.service;
 
 import com.ceos19.springeverytime.post.domain.Post;
+import com.ceos19.springeverytime.post.repository.PostRepository;
 import com.ceos19.springeverytime.post.service.PostService;
 import com.ceos19.springeverytime.postlike.domain.PostLike;
 import com.ceos19.springeverytime.postlike.dto.PostLikeDto;
 import com.ceos19.springeverytime.postlike.repository.PostLikeRepository;
 import com.ceos19.springeverytime.user.domain.User;
-import com.ceos19.springeverytime.user.service.UserService;
+import com.ceos19.springeverytime.user.repository.UserRepository;
 import jakarta.persistence.EntityNotFoundException;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,27 +19,38 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class PostLikeService {
-    private PostLikeRepository postLikeRepository;
-    private PostService postService;
-    private UserService userService;
+    private final PostLikeRepository postLikeRepository;
+    private final PostService postService;
+    private final PostRepository postRepository;
+    private final UserRepository userRepository;
 
     @Transactional
-    public void createLike(PostLikeDto postLikeDto){
-        User user = userService.getUser(postLikeDto.getUserId());
-        Post post = postService.getPost(postLikeDto.getPostId());
+    public Long createLike(PostLikeDto postLikeDto){
+        User user = userRepository.findUserById(postLikeDto.getUserId())
+                .orElseThrow(EntityNotFoundException::new);
+        Post post = postRepository.findPostById(postLikeDto.getPostId())
+                .orElseThrow(EntityNotFoundException::new);
 
         postService.increaseLike(post.getId());
-        postLikeRepository.save(postLikeDto.toEntity(user,post));
+        PostLike save = postLikeRepository.save(postLikeDto.toEntity(user, post));
+
+        return save.getId();
     }
 
-    public PostLike getLike(Long postLikeId){
-        return postLikeRepository.findById(postLikeId)
-                .orElseThrow(EntityNotFoundException::new);
+    public PostLikeDto getLikeByPostAndUser(Long postId, Long userId) throws NotFoundException {
+        PostLike postLike = postLikeRepository.findByUser_IdAndPost_Id(userId, postId)
+                .orElseThrow(NotFoundException::new);
+
+        return PostLikeDto.of(postLike);
+    }
+
+    public List<PostLike> getAllLikeByPost(Long postId){
+        return postLikeRepository.findByPostId(postId);
     }
 
     @Transactional
-    public void deleteLike(Long postLikeId){
-        postService.decreaseLike(postLikeId);
-        postLikeRepository.deleteById(postLikeId);
+    public void deleteLike(PostLikeDto postLikeDto){
+        postService.decreaseLike(postLikeDto.getPostId());
+        postLikeRepository.deleteById(postLikeDto.getId());
     }
 }
