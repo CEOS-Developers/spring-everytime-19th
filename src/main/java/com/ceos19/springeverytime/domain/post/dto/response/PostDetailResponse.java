1a88ef2fb954e5adc1c8a6b9050a4724f9f7db2f
kwon <kckc0608@naver.com>
2024-03-30 19:09:05 +0900

    fix: 게시글 단건 조회가 안되는 문제 해결

--- src/main/java/com/ceos19/springeverytime/domain/post/dto/response/PostDetailResponse.java
+++ src/main/java/com/ceos19/springeverytime/domain/post/dto/response/PostDetailResponse.java
@@ -3,11 +3,13 @@ package com.ceos19.springeverytime.domain.post.dto.response;
 import com.ceos19.springeverytime.domain.comment.dto.response.CommentResponse;
 import com.ceos19.springeverytime.domain.post.domain.Post;
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Getter
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class PostDetailResponse {
     private final Long id;
