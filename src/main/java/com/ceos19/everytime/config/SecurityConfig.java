975a417b887e77026f41be7f699d3d109eccc0ff
leeedohyun <ldh19738@gmail.com>
2024-04-15 18:06:52 +0900

    feat: 시큐리티 설정 추가

---
+++ src/main/java/com/ceos19/everytime/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.ceos19.everytime.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable);
+
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/**").permitAll());
+
+        return http.build();
+    }
+}
