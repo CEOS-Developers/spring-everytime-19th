30bbd468d0eb4a68e8d43640d0d7d1ce82d78248
Min Jeon <mulsoap0504@gmail.com>
2024-04-17 16:17:06 +0900

    feat: 람다 추가 리팩토링, 예외추가
    addBoard 메서드 파라미터 변경

--- src/main/java/com/ceos19/everytime/service/BoardService.java
+++ src/main/java/com/ceos19/everytime/service/BoardService.java
@@ -1,9 +1,11 @@
 package com.ceos19.everytime.service;
 
 import com.ceos19.everytime.domain.Board;
+import com.ceos19.everytime.domain.School;
 import com.ceos19.everytime.exception.AppException;
 import com.ceos19.everytime.exception.ErrorCode;
 import com.ceos19.everytime.repository.BoardRepository;
+import com.ceos19.everytime.repository.SchoolRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -22,17 +24,25 @@ import static com.ceos19.everytime.exception.ErrorCode.NO_DATA_EXISTED;
 @Slf4j
 public class BoardService {
     private final BoardRepository boardRepository;
+    private final SchoolRepository schoolRepository;
 
     @Transactional(readOnly = false)
-    public Long addBoard(Board board) {
-        boardRepository.findBySchoolIdAndName(board.getSchool().getId(), board.getName())
-                .orElseThrow(() -> {
-                    log.error("에러 내용: 게시판 등록 실패 " +
-                            "발생 원인: 이미 존재하는 게시판명으로 등록 시도");
-                    return new AppException(DATA_ALREADY_EXISTED, "이미 존재하는 게시판입니다");
-                });
+    public Board addBoard(String name, Long schoolId) {
+        School school = schoolRepository.findById(schoolId).orElseThrow(() -> {
+            log.error("에러 내용: 게시판 등록 실패 " +
+                    "발생 원인: 존재하지 않는 School PK로 조회");
+            return new AppException(NO_DATA_EXISTED, "존재하지 않는 학교입니다");
+        });
+        boardRepository.findBySchoolIdAndName(schoolId, name).ifPresent(s -> {
+            log.error("에러 내용: 게시판 등록 실패 " +
+                    "발생 원인: 이미 존재하는 게시판명으로 등록 시도");
+            throw new AppException(DATA_ALREADY_EXISTED, "이미 존재하는 게시판입니다");
+        });
+
+        Board board = new Board(name, school);
         boardRepository.save(board);
-        return board.getId();
+
+        return board;
     }
 
     public Board findBoardById(Long boardId) {
@@ -46,10 +56,20 @@ public class BoardService {
     }
 
     public List<Board> findBoardBySchoolId(Long schoolId) {
+        if (schoolRepository.findById(schoolId).isEmpty()) {
+            log.error("에러 내용: 게시판 조회 실패 " +
+                    "발생 원인: 존재하지 않는 학교 PK로 조회");
+            throw new AppException(DATA_ALREADY_EXISTED, "존재하지 않는 학교입니다");
+        }
         return boardRepository.findBySchoolId(schoolId);
     }
 
     public boolean isBoardExist(Long schoolId, String name) {
+        if (schoolRepository.findById(schoolId).isEmpty()) {
+            log.error("에러 내용: 게시판 조회 실패 " +
+                    "발생 원인: 존재하지 않는 학교 PK로 조회");
+            throw new AppException(DATA_ALREADY_EXISTED, "존재하지 않는 학교입니다");
+        }
         return boardRepository.findBySchoolIdAndName(schoolId, name).isPresent();
     }
 
@@ -72,12 +92,12 @@ public class BoardService {
                             return new AppException(NO_DATA_EXISTED, "존재하지 않는 게시판입니다");
                         }
                 );
-        boardRepository.findBySchoolIdAndName(board.getId(),name)
-                        .ifPresent(b -> {
-                            log.error("에러 내용: 게시판 수정 실패 " +
-                                    "발생 원인: 이미 존재하는 게시판 이름으로 수정 시도");
-                            throw new AppException(DATA_ALREADY_EXISTED, "이미 사용중인 게시판 이름입니다");
-                        });
+        boardRepository.findBySchoolIdAndName(board.getId(), name)
+                .ifPresent(b -> {
+                    log.error("에러 내용: 게시판 수정 실패 " +
+                            "발생 원인: 이미 존재하는 게시판 이름으로 수정 시도");
+                    throw new AppException(DATA_ALREADY_EXISTED, "이미 사용중인 게시판 이름입니다");
+                });
         board.updateName(name);
     }
 }
