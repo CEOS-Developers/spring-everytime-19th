90a1981b11a0a0b1725ccac3caff085a61706a27
Min Jeon <mulsoap0504@gmail.com>
2024-04-17 16:10:47 +0900

    feat: controller 수정
    1) 도메인을 단건일 시 school 다중일 때 schools로 구분
    2)ResponseEntity로 HttpStatus가 반환되도록 설정.
    3)컨트롤러단과 서비스로직 구분

--- src/main/java/com/ceos19/everytime/controller/api/SchoolController.java
+++ src/main/java/com/ceos19/everytime/controller/api/SchoolController.java
@@ -12,6 +12,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -19,127 +20,209 @@ import java.util.List;
 
 @Slf4j
 @RestController
-@RequestMapping("/api/schools")
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class SchoolController {
     private final SchoolService schoolService;
     private final BoardService boardService;
     private final CourseService courseService;
 
-    @PostMapping
-    public BaseResponse addSchool(@Valid @RequestBody AddSchoolRequest request) {
-        School school = new School(request.getName());
+    /**
+     * 학교 등록
+     *
+     * @param request
+     * @return
+     */
+    @PostMapping("/school")
+    public ResponseEntity<BaseResponse> addSchool(@Valid @RequestBody AddSchoolRequest request) {
         try {
-            Long id = schoolService.addSchool(school);
-            return new BaseResponse(HttpStatus.OK, null, id, 1);
+            Long id = schoolService.addSchool(request.getName());
+
+            return ResponseEntity.ok(new BaseResponse(HttpStatus.OK, null, id, 1));
         } catch (AppException e) {
-            return new BaseResponse(e.getErrorCode().getHttpStatus(), e.getMessage(), null, 0);
+            BaseResponse response =
+                    new BaseResponse(e.getErrorCode().getHttpStatus(), e.getMessage(), null, 0);
+            return new ResponseEntity<>(response, e.getErrorCode().getHttpStatus());
         }
     }
 
-    @GetMapping("/{sid}")
-    public BaseResponse<ReadSchoolResponse> readSchool(@PathVariable("sid") Long schoolId) {
+    /**
+     * PK로 학교 조회
+     *
+     * @param schoolId
+     * @return
+     */
+    @GetMapping("/school/{school_id}")
+    public ResponseEntity<BaseResponse<ReadSchoolResponse>> readSchool(@PathVariable("school_id") Long schoolId) {
         try {
             School school = schoolService.findSchoolById(schoolId);
-            ReadSchoolResponse readSchoolResponse = ReadSchoolResponse.from(school);
-            return new BaseResponse<>(HttpStatus.OK, null, readSchoolResponse, 1);
+            ReadSchoolResponse value = ReadSchoolResponse.from(school);
+
+            return ResponseEntity.ok(new BaseResponse<>(HttpStatus.OK, null, value, 1));
         } catch (AppException e) {
-            return new BaseResponse<>(e.getErrorCode().getHttpStatus(), e.getMessage(), null, 0);
+            BaseResponse response =
+                    new BaseResponse(e.getErrorCode().getHttpStatus(), e.getMessage(), null, 0);
+            return new ResponseEntity<>(response, e.getErrorCode().getHttpStatus());
         }
     }
 
-    @GetMapping
-    public BaseResponse<List<ReadSchoolResponse>> readSchool(@RequestParam(value = "name", required = false) String name) {
+    /**
+     * 이름으로 학교 조회
+     *
+     * @param name
+     * @return
+     */
+    @GetMapping("/school")
+    public ResponseEntity<BaseResponse<ReadSchoolResponse>> readSchoolResponseBaseResponse(@RequestParam(value = "name") String name) {
+        try {
+            School school = schoolService.findSchoolByName(name);
+            ReadSchoolResponse value = ReadSchoolResponse.from(school);
+
+            return ResponseEntity.ok(new BaseResponse(HttpStatus.OK, null, value, 1));
+        } catch (AppException e) {
+            BaseResponse response =
+                    new BaseResponse(e.getErrorCode().getHttpStatus(), e.getMessage(), null, 0);
+            return new ResponseEntity<>(response, e.getErrorCode().getHttpStatus());
+        }
+    }
+
+    /**
+     * 모든 학교 조회
+     *
+     * @return
+     */
+    @GetMapping("/schools")// 전체 조회
+    public ResponseEntity<BaseResponse<List<ReadSchoolResponse>>> readSchool() {
         try {
             List<ReadSchoolResponse> value = new ArrayList<>();
-            if (name == null) {
-                // 전체 조회
-                List<School> schools = schoolService.findSchools();
-                schools.forEach(school -> value.add(ReadSchoolResponse.from(school)));
-            } else {
-                // 이름으로 단건 조회
-                School school = schoolService.findSchoolByName(name);
-                ReadSchoolResponse readSchoolResponse = ReadSchoolResponse.from(school);
-                value.add(readSchoolResponse);
-            }
+            List<School> schools = schoolService.findSchools();
+            schools.forEach(school -> value.add(ReadSchoolResponse.from(school)));
 
-            return new BaseResponse<>(HttpStatus.OK, null, value, value.size());
-
-
+            return ResponseEntity.ok(new BaseResponse(HttpStatus.OK, null, value, value.size()));
         } catch (AppException e) {
-            return new BaseResponse<>(e.getErrorCode().getHttpStatus(), e.getMessage(), null, 0);
+            BaseResponse response =
+                    new BaseResponse(e.getErrorCode().getHttpStatus(), e.getMessage(), null, 0);
+            return new ResponseEntity<>(response, e.getErrorCode().getHttpStatus());
         }
     }
 
-    @PatchMapping("/{sid}")
-    public BaseResponse modifySchool(@PathVariable("sid") Long schoolId, @Valid @RequestBody ModifySchoolRequest request) {
+    /**
+     * 학교명 수정
+     *
+     * @param schoolId
+     * @param request
+     * @return
+     */
+    @PatchMapping("/school/{school_id}")
+    public ResponseEntity<BaseResponse> modifySchool(@PathVariable("school_id") Long schoolId, @Valid @RequestBody ModifySchoolRequest request) {
         try {
             schoolService.modifySchool(schoolId, request.getName());
-            return new BaseResponse(HttpStatus.OK, null, null, 0);
+
+            return ResponseEntity.ok(new BaseResponse(HttpStatus.OK, null, null, 0));
         } catch (AppException e) {
-            return new BaseResponse<>(e.getErrorCode().getHttpStatus(), e.getMessage(), null, 0);
+            BaseResponse response =
+                    new BaseResponse(e.getErrorCode().getHttpStatus(), e.getMessage(), null, 0);
+            return new ResponseEntity<>(response, e.getErrorCode().getHttpStatus());
         }
     }
 
-    @PostMapping("/{sid}/boards")
-    public BaseResponse addBoard(@PathVariable("sid") Long schoolId, @Valid @RequestBody AddBoardRequest request) {
+    /**
+     * 학교에 게시판 등록
+     *
+     * @param schoolId
+     * @param request
+     * @return
+     */
+    @PostMapping("/school/{school_id}/board")
+    public ResponseEntity<BaseResponse> addBoard(@PathVariable("school_id") Long schoolId, @Valid @RequestBody AddBoardRequest request) {
+        try {
+            Board board = boardService.addBoard(request.getName(), schoolId);
+
+            return ResponseEntity.ok(new BaseResponse(HttpStatus.OK, null, board.getId(), 1));
+        } catch (AppException e) {
+            BaseResponse response =
+                    new BaseResponse(e.getErrorCode().getHttpStatus(), e.getMessage(), null, 0);
+            return new ResponseEntity<>(response, e.getErrorCode().getHttpStatus());
+        }
+    }
+
+    /**
+     * 학교에 있는 모든 게시판 조회
+     *
+     * @param schoolId
+     * @return
+     */
+    @GetMapping("/school/{school_id}/boards")
+    public ResponseEntity<BaseResponse<List<ReadBoardResponse>>> readBoards(@PathVariable("school_id") Long schoolId) {
+        List<ReadBoardResponse> value = new ArrayList<>();
+        try {
+            List<Board> boards = boardService.findBoardBySchoolId(schoolId);
+            boards.forEach(board -> value.add(ReadBoardResponse.from(board)));
+            return ResponseEntity.ok(new BaseResponse<>(HttpStatus.OK, null, value, value.size()));
+        } catch (AppException e) {
+            BaseResponse response =
+                    new BaseResponse(e.getErrorCode().getHttpStatus(), e.getMessage(), null, 0);
+            return new ResponseEntity<>(response, e.getErrorCode().getHttpStatus());
+        }
+    }
+
+    /**
+     * 이름으로 교내게시판 조회
+     *
+     * @param schoolId
+     * @param name
+     * @return
+     */
+    @GetMapping("/school/{school_id}/board")
+    public ResponseEntity<BaseResponse<ReadBoardResponse>> readBoard(@PathVariable("school_id") Long schoolId,
+                                                                     @RequestParam(value = "name") String name) {
+        try {
+            // 이름으로 게시판 단건 조회
+            Board board = boardService.findBoardBySchoolIdAndName(schoolId, name);
+            ReadBoardResponse value = ReadBoardResponse.from(board);
+
+            return ResponseEntity.ok(new BaseResponse<>(HttpStatus.OK, null, value, 1));
+        } catch (AppException e) {
+            BaseResponse response =
+                    new BaseResponse(e.getErrorCode().getHttpStatus(), e.getMessage(), null, 0);
+            return new ResponseEntity<>(response, e.getErrorCode().getHttpStatus());
+        }
+    }
+
+    /**
+     * 학교에 수업 등록
+     *
+     * @param schoolId
+     * @param request
+     * @return
+     */
+    @PostMapping("/school/{school_id}/course")
+    public ResponseEntity<BaseResponse> addCourse(@PathVariable("school_id") Long schoolId,
+                                  @Valid @RequestBody AddCourseRequest request) {
         try {
             School school = schoolService.findSchoolById(schoolId);
-            Board board = new Board(request.getName(), school);
-            Long id = boardService.addBoard(board);
+            Long courseId = courseService.addCourse(request, schoolId);
 
-            return new BaseResponse<>(HttpStatus.OK, null, id, 1);
+            return ResponseEntity.ok(new BaseResponse(HttpStatus.OK, null, courseId, 1));
         } catch (AppException e) {
-            return new BaseResponse<>(e.getErrorCode().getHttpStatus(), e.getMessage(), null, 0);
+            BaseResponse response =
+                    new BaseResponse(e.getErrorCode().getHttpStatus(), e.getMessage(), null, 0);
+            return new ResponseEntity<>(response, e.getErrorCode().getHttpStatus());
         }
     }
 
-    @GetMapping("/{sid}/boards")
-    public BaseResponse<List<ReadBoardResponse>> readBoard(@PathVariable("sid") Long schoolId, @RequestParam(value = "name", required = false) String name) {
-        try {
-            List<ReadBoardResponse> value = new ArrayList<>();
-            if (name == null) {
-                // 전체 조회
-                List<Board> boards = boardService.findBoardBySchoolId(schoolId);
-                boards.forEach(board -> value.add(ReadBoardResponse.from(board)));
-            } else {
-                // 이름으로 단건 조회
-                Board board = boardService.findBoardBySchoolIdAndName(schoolId, name);
-                value.add(ReadBoardResponse.from(board));
-            }
-
-            return new BaseResponse<>(HttpStatus.OK, null, value, value.size());
-        } catch (AppException e) {
-            return new BaseResponse<>(e.getErrorCode().getHttpStatus(), e.getMessage(), null, 0);
-        }
-    }
-
-    @PostMapping("/{sid}/courses")
-    public BaseResponse addCourse(@PathVariable("sid") Long schoolId, @Valid @RequestBody AddCourseRequest request) {
-        try {
-            School school = schoolService.findSchoolById(schoolId);
-
-            Course course = Course.builder()
-                    .name(request.getName())
-                    .courseNumber(request.getCourseNumber())
-                    .room(request.getRoom())
-                    .openingGrade(request.getOpeningGrade())
-                    .professorName(request.getProfessorName())
-                    .credit(request.getCredit())
-                    .school(school)
-                    .build();
-            Long id = courseService.addCourse(course);
-
-            return new BaseResponse(HttpStatus.OK, null, id, 1);
-        } catch (AppException e) {
-            return new BaseResponse<>(e.getErrorCode().getHttpStatus(), e.getMessage(), null, 0);
-        }
-    }
-
-    @GetMapping("/{sid}/courses")
-    public BaseResponse<List<ReadCourseResponse>> readCourse(@PathVariable("sid") Long schoolId,
-                                                             @RequestParam(value = "name",required = false) String name,
-                                                             @RequestParam(value = "professorName",required = false) String professorName) {
+    /**
+     * 수업명, 교수명으로 교내 수업 조회
+     *
+     * @param schoolId
+     * @param name
+     * @param professorName
+     * @return
+     */
+    @GetMapping("/school/{school_id}/courses")
+    public ResponseEntity<BaseResponse<List<ReadCourseResponse>>> readCourse(@PathVariable("school_id") Long schoolId,
+                                                             @RequestParam(value = "name", required = false) String name,
+                                                             @RequestParam(value = "professorName", required = false) String professorName) {
         try {
             List<ReadCourseResponse> value = new ArrayList<>();
 
@@ -165,10 +248,11 @@ public class SchoolController {
                 courses.forEach(course -> value.add(ReadCourseResponse.from(course)));
             }
 
-            return new BaseResponse<>(HttpStatus.OK, null, value, value.size());
+            return ResponseEntity.ok(new BaseResponse<>(HttpStatus.OK, null, value, value.size()));
         } catch (AppException e) {
-            return new BaseResponse<>(e.getErrorCode().getHttpStatus(), e.getMessage(), null, 0);
+            BaseResponse response =
+                    new BaseResponse(e.getErrorCode().getHttpStatus(), e.getMessage(), null, 0);
+            return new ResponseEntity<>(response, e.getErrorCode().getHttpStatus());
         }
-
     }
 }
