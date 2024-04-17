54bb9b7e0d7df43bc8ccd973b31667db5cd88e92
Min Jeon <mulsoap0504@gmail.com>
2024-04-17 16:17:22 +0900

    feat: 람다 추가 리팩토링

--- src/main/java/com/ceos19/everytime/service/TimeTableService.java
+++ src/main/java/com/ceos19/everytime/service/TimeTableService.java
@@ -27,17 +27,17 @@ public class TimeTableService {
 
     public Long addTimeTable(TimeTable timeTable) {
         List<TimeTable> timeTables = timeTableRepository.findByUserId(timeTable.getUser().getId());
-        if (timeTableRepository.findByUserIdAndYearAndSemesterAndName
-                (timeTable.getUser().getId(),
-                        timeTable.getYear(),
-                        timeTable.getSemester(),
-                        timeTable.getName())
-                .isPresent())
-        {
-            log.error("에러 내용: 시간표 생성 불가 " +
-                    "발생 원인: 중복된 시간표 등록");
-            throw new AppException(DATA_ALREADY_EXISTED, "중복된 시간표입니다");
-        }
+        timeTableRepository.findByUserIdAndYearAndSemesterAndName
+                        (timeTable.getUser().getId(),
+                                timeTable.getYear(),
+                                timeTable.getSemester(),
+                                timeTable.getName())
+                .ifPresent(f -> {
+                    log.error("에러 내용: 시간표 생성 불가 " +
+                            "발생 원인: 중복된 시간표 등록");
+                    throw new AppException(DATA_ALREADY_EXISTED, "중복된 시간표입니다");
+                });
+
 
         timeTableRepository.save(timeTable);
         return timeTable.getId();
@@ -45,44 +45,51 @@ public class TimeTableService {
 
     @Transactional(readOnly = true)
     public TimeTable findTimeTableById(Long timeTableId) {
-        Optional<TimeTable> optionalTimeTable = timeTableRepository.findById(timeTableId);
-        if (optionalTimeTable.isEmpty()) {
+        return timeTableRepository.findById(timeTableId).orElseThrow(() -> {
             log.error("에러 내용: 시간표 조회 실패 " +
                     "발생 원인: 존재하지 않는 PK 값으로 조회");
-            throw new AppException(NO_DATA_EXISTED, "존재하지 않는 시간표입니다");
-        }
-        return optionalTimeTable.get();
+            return new AppException(NO_DATA_EXISTED, "존재하지 않는 시간표입니다");
+        });
     }
 
     @Transactional(readOnly = true)
     public List<TimeTable> findTimeTableByUserId(Long userId) {
+        userRepository.findById(userId).orElseThrow(() -> {
+            log.error("에러 내용: 시간표 조회 실패 " +
+                    "발생 원인: 존재하지 않는 User의 PK 값으로 조회");
+            return new AppException(NO_DATA_EXISTED, "존재하지 않는 유저입니다");
+        });
+
         return timeTableRepository.findByUserId(userId);
     }
 
     @Transactional(readOnly = true)
     public List<TimeTable> findTimeTableByUserIdAndYearAndSemester(Long userId, int year, Semester semester) {
+        userRepository.findById(userId).orElseThrow(() -> {
+            log.error("에러 내용: 시간표 조회 실패 " +
+                    "발생 원인: 존재하지 않는 User의 PK 값으로 조회");
+            return new AppException(NO_DATA_EXISTED, "존재하지 않는 유저입니다");
+        });
         return timeTableRepository.findByUserIdAndYearAndSemester(userId, year, semester);
     }
 
     @Transactional(readOnly = true)
     public TimeTable findTimeTableByUserIdAndYearAndSemesterAndName(Long userId, int year, Semester semester, String name) {
-        Optional<TimeTable> optionalTimeTable = timeTableRepository.findByUserIdAndYearAndSemesterAndName(userId, year, semester, name);
-        if (optionalTimeTable.isEmpty()) {
-            log.error("에러 내용: 시간표 조회 실패 " +
-                    "발생 원인: 조건에 맞는 시간표 존재 안함");
-            throw new AppException(NO_DATA_EXISTED, "존재하지 않는 시간표입니다");
-        }
-        return optionalTimeTable.get();
+        return timeTableRepository.findByUserIdAndYearAndSemesterAndName(userId, year, semester, name)
+                .orElseThrow(() -> {
+                    log.error("에러 내용: 시간표 조회 실패 " +
+                            "발생 원인: 조건에 맞는 시간표 존재 안함");
+                    return new AppException(NO_DATA_EXISTED, "존재하지 않는 시간표입니다");
+                });
     }
 
 
     public void removeTimeTable(Long timeTableId) {
-        Optional<TimeTable> optionalTimeTable = timeTableRepository.findById(timeTableId);
-        if (optionalTimeTable.isEmpty()) {
+        timeTableRepository.findById(timeTableId).orElseThrow(() -> {
             log.error("에러 내용: 시간표 조회 실패 " +
                     "발생 원인: 존재하지 않는 PK 값으로 조회");
             throw new AppException(NO_DATA_EXISTED, "존재하지 않는 시간표입니다");
-        }
+        });
 
         // 연관관계 제거
         timeTableCourseRepository.deleteAllByTimeTableId(timeTableId);
