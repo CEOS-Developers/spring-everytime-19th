ad2ae37d05a9552ff5dad8ae55d6a8f626e36e13
riceCakeSsamanKo <mulsoap0504@gmail.com>
2024-04-19 17:38:22 +0900

    feat: 컨틀롤러 로직 서비스 분리

--- src/main/java/com/ceos19/everytime/service/CommentService.java
+++ src/main/java/com/ceos19/everytime/service/CommentService.java
@@ -1,8 +1,13 @@
 package com.ceos19.everytime.service;
 
 import com.ceos19.everytime.domain.Comment;
+import com.ceos19.everytime.domain.Post;
+import com.ceos19.everytime.domain.User;
+import com.ceos19.everytime.dto.AddCommentRequest;
 import com.ceos19.everytime.exception.AppException;
 import com.ceos19.everytime.repository.CommentRepository;
+import com.ceos19.everytime.repository.PostRepository;
+import com.ceos19.everytime.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -19,10 +24,38 @@ import static com.ceos19.everytime.exception.ErrorCode.NO_DATA_EXISTED;
 @Slf4j
 public class CommentService {
     private final CommentRepository commentRepository;
+    private final PostRepository postRepository;
+    private final UserRepository userRepository;
 
     public Long addComment(Comment comment) {
         commentRepository.save(comment);
         return comment.getId();
+    }
+
+    public Comment addComment(AddCommentRequest request, Long postId) {
+        User commenter = userRepository.findById(request.getCommenterId()).orElseThrow(() -> {
+            log.error("에러 내용: 댓글 조회 실패 " +
+                    "발생 원인: 존재하지 않는 User의 PK 값으로 조회");
+            return new AppException(NO_DATA_EXISTED, "존재하지 않는 유저입니다");
+        });
+        Post post = postRepository.findById(postId).orElseThrow(() -> {
+            log.error("에러 내용: 게시물 조회 실패 " +
+                    "발생 원인: 존재하지 않는 Post의 PK 값으로 조회");
+            return new AppException(NO_DATA_EXISTED, "존재하지 않는 게시물입니다");
+        });
+
+        Long parentCommentId = request.getParentCommentId();
+        Comment parentComment = null;
+        if (parentCommentId != null)
+            parentComment = commentRepository.findById(parentCommentId).orElseThrow(() -> {
+                log.error("에러 내용: 게시물 조회 실패 " +
+                        "발생 원인: 존재하지 않는 Comment의 PK 값으로 조회");
+                return new AppException(NO_DATA_EXISTED, "대댓글을 달 수 있는 댓글이 존재하지 않습니다");
+            });
+
+        Comment comment = new Comment(request.getContent(), commenter, post, parentComment);
+        commentRepository.save(comment);
+        return comment;
     }
 
     @Transactional(readOnly = true)
@@ -38,16 +71,31 @@ public class CommentService {
 
     @Transactional(readOnly = true)
     public List<Comment> findCommentByPostId(Long postId) {
+        if (postRepository.findById(postId).isEmpty()) {
+            log.error("에러 내용: 댓글 조회 실패 " +
+                    "발생 원인: 존재하지 않는 Post의 PK 값으로 조회");
+            throw new AppException(NO_DATA_EXISTED, "존재하지 않는 게시물입니다");
+        }
         return commentRepository.findByPostId(postId);
     }
 
     @Transactional(readOnly = true)
     public List<Comment> findCommentReplies(Long commentId) {
+        if (commentRepository.findById(commentId).isEmpty()) {
+            log.error("에러 내용: 대댓글 조회 실패 " +
+                    "발생 원인: 존재하지 않는 Comment의 PK 값으로 조회");
+            throw new AppException(NO_DATA_EXISTED, "존재하지 않는 댓글입니다");
+        }
         return commentRepository.findByParentCommentId(commentId);
     }
 
     @Transactional(readOnly = true)
     public List<Comment> findCommentByCommenterId(Long commenterId) {
+        if (userRepository.findById(commenterId).isEmpty()) {
+            log.error("에러 내용: 댓글 조회 실패 " +
+                    "발생 원인: 존재하지 않는 User의 PK 값으로 조회");
+            throw new AppException(NO_DATA_EXISTED, "존재하지 않는 유저입니다");
+        }
         return commentRepository.findByCommenterId(commenterId);
     }
 
