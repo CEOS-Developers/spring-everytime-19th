e1828458a41928286403b624b089a24705890b9b
riceCakeSsamanKo <mulsoap0504@gmail.com>
2024-04-24 16:42:34 +0900

    feat: request exception handler 생성
    request에 Valid 관련 오류가 발생시 처리해주는 exception handler

---
+++ src/main/java/com/ceos19/everytime/handler/ApiControllerAdvice.java
@@ -0,0 +1,27 @@
+package com.ceos19.everytime.handler;
+
+import com.ceos19.everytime.dto.BaseResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class ApiControllerAdvice {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)  // @Valid에서 에러가 발생한 경우 여기에서 처리
+    public ResponseEntity<BaseResponse<Map<String, String>>> handleValidationExceptions(MethodArgumentNotValidException ex){
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors()
+                .forEach(c -> errors.put(((FieldError) c).getField(), c.getDefaultMessage()));
+
+        return ResponseEntity.badRequest()
+                .body(new BaseResponse<>(HttpStatus.BAD_REQUEST, "request parameter error", errors, 0));
+    }
+
+}
