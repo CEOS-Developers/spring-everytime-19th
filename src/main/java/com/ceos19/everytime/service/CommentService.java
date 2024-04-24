03a5541a1a732b797a6cd8ebf2a280f01cd21737
riceCakeSsamanKo <mulsoap0504@gmail.com>
2024-04-24 16:44:12 +0900

    refactor: lambda 사용 리팩토링

--- src/main/java/com/ceos19/everytime/service/CommentService.java
+++ src/main/java/com/ceos19/everytime/service/CommentService.java
@@ -27,6 +27,9 @@ public class CommentService {
     private final PostRepository postRepository;
     private final UserRepository userRepository;
 
+    /**
+     * Deprecated
+     */
     public Long addComment(Comment comment) {
         commentRepository.save(comment);
         return comment.getId();
@@ -60,53 +63,52 @@ public class CommentService {
 
     @Transactional(readOnly = true)
     public Comment findCommentById(Long commentId) {
-        Optional<Comment> optionalComment = commentRepository.findById(commentId);
-        if (optionalComment.isEmpty()) {
+        return commentRepository.findById(commentId).orElseThrow(() -> {
             log.error("에러 내용: 댓글 조회 실패 " +
                     "발생 원인: 존재하지 않는 PK 값으로 조회");
-            throw new AppException(NO_DATA_EXISTED, "존재하지 않는 댓글입니다");
-        }
-        return optionalComment.get();
+            return new AppException(NO_DATA_EXISTED, "존재하지 않는 댓글입니다");
+        });
     }
 
     @Transactional(readOnly = true)
     public List<Comment> findCommentByPostId(Long postId) {
-        if (postRepository.findById(postId).isEmpty()) {
+        postRepository.findById(postId).orElseThrow(() -> {
             log.error("에러 내용: 댓글 조회 실패 " +
                     "발생 원인: 존재하지 않는 Post의 PK 값으로 조회");
-            throw new AppException(NO_DATA_EXISTED, "존재하지 않는 게시물입니다");
-        }
+            return new AppException(NO_DATA_EXISTED, "존재하지 않는 게시물입니다");
+        });
+
         return commentRepository.findByPostId(postId);
     }
 
     @Transactional(readOnly = true)
     public List<Comment> findCommentReplies(Long commentId) {
-        if (commentRepository.findById(commentId).isEmpty()) {
+        commentRepository.findById(commentId).orElseThrow(() -> {
             log.error("에러 내용: 대댓글 조회 실패 " +
                     "발생 원인: 존재하지 않는 Comment의 PK 값으로 조회");
-            throw new AppException(NO_DATA_EXISTED, "존재하지 않는 댓글입니다");
-        }
+            return new AppException(NO_DATA_EXISTED, "존재하지 않는 댓글입니다");
+        });
+
         return commentRepository.findByParentCommentId(commentId);
     }
 
     @Transactional(readOnly = true)
     public List<Comment> findCommentByCommenterId(Long commenterId) {
-        if (userRepository.findById(commenterId).isEmpty()) {
+        userRepository.findById(commenterId).orElseThrow(() -> {
             log.error("에러 내용: 댓글 조회 실패 " +
                     "발생 원인: 존재하지 않는 User의 PK 값으로 조회");
-            throw new AppException(NO_DATA_EXISTED, "존재하지 않는 유저입니다");
-        }
+            return new AppException(NO_DATA_EXISTED, "존재하지 않는 유저입니다");
+        });
         return commentRepository.findByCommenterId(commenterId);
     }
 
 
     public void removeComment(Long commentId) {
-        Optional<Comment> optionalComment = commentRepository.findById(commentId);
-        if (optionalComment.isEmpty()) {
+        commentRepository.findById(commentId).orElseThrow(() -> {
             log.error("에러 내용: 댓글 조회 실패 " +
                     "발생 원인: 존재하지 않는 PK 값으로 조회");
-            throw new AppException(NO_DATA_EXISTED, "존재하지 않는 댓글입니다");
-        }
+            return new AppException(NO_DATA_EXISTED, "존재하지 않는 댓글입니다");
+        });
 
         List<Comment> replies = commentRepository.findByParentCommentId(commentId);
         // 연관관계 제거
