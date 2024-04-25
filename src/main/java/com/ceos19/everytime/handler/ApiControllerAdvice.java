269c249a175550e2345bc98f8e5ae97c3145024c
riceCakeSsamanKo <mulsoap0504@gmail.com>
2024-04-26 00:19:47 +0900

    feat: exception handler구현

--- src/main/java/com/ceos19/everytime/handler/ApiControllerAdvice.java
+++ src/main/java/com/ceos19/everytime/handler/ApiControllerAdvice.java
@@ -4,9 +4,11 @@ import com.ceos19.everytime.dto.BaseResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
+import org.springframework.validation.method.ParameterValidationResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.HandlerMethodValidationException;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -23,5 +25,16 @@ public class ApiControllerAdvice {
         return ResponseEntity.badRequest()
                 .body(new BaseResponse<>(HttpStatus.BAD_REQUEST, "request parameter error", errors, 0));
     }
+
+    @ExceptionHandler(HandlerMethodValidationException.class)  // @Valid에서 에러가 발생한 경우 여기에서 처리
+    public ResponseEntity<BaseResponse<Map<String, String>>> handleValidationExceptions(HandlerMethodValidationException ex){
+        for (ParameterValidationResult result : ex.getValueResults()) {
+            System.out.println("result = " + result);
+        }
+
+        return ResponseEntity.badRequest()
+                .body(new BaseResponse<>(HttpStatus.BAD_REQUEST, "request parameter error", null, 0));
+    }
+
 
 }
