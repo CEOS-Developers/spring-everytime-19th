9629b2ef90c5d9461f8e36b0c9fe7b8b67f2dae4
riceCakeSsamanKo <mulsoap0504@gmail.com>
2024-04-27 21:30:39 +0900

    rename: dto 이름 변경

--- src/main/java/com/ceos19/everytime/controller/api/UserController.java
+++ src/main/java/com/ceos19/everytime/controller/api/UserController.java
@@ -1,19 +1,14 @@
 package com.ceos19.everytime.controller.api;
 
 import com.ceos19.everytime.domain.User;
-import com.ceos19.everytime.dto.AddUserRequest;
-import com.ceos19.everytime.dto.BaseResponse;
-import com.ceos19.everytime.dto.ReadUserParam;
-import com.ceos19.everytime.dto.ReadUserResponse;
+import com.ceos19.everytime.dto.*;
 import com.ceos19.everytime.exception.AppException;
 import com.ceos19.everytime.exception.ErrorCode;
 import com.ceos19.everytime.service.UserService;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.Email;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -26,16 +21,18 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/join")
-    public ResponseEntity<BaseResponse> join(@Valid @RequestBody AddUserRequest request) {
-        try{
-            User user = userService.addUser(request);
+    public ResponseEntity<BaseResponse> join(@Valid @RequestBody JoinUserRequest request) {
+        try {
+            User user = userService.join(request);
             return ResponseEntity.ok(new BaseResponse(HttpStatus.OK, null, user.getId(), 1));
-        }catch (AppException e) {
+        } catch (AppException e) {
             BaseResponse response =
                     new BaseResponse(e.getErrorCode().getHttpStatus(), e.getMessage(), null, 0);
             return new ResponseEntity<>(response, e.getErrorCode().getHttpStatus());
         }
     }
+
+
 
     @GetMapping("/user/{user_id}")
     public ResponseEntity<BaseResponse<ReadUserResponse>> readUser(@PathVariable("user_id") Long userId) {
--- src/main/java/com/ceos19/everytime/dto/JoinUserRequest.java
+++ src/main/java/com/ceos19/everytime/dto/JoinUserRequest.java
@@ -1,22 +1,13 @@
 package com.ceos19.everytime.dto;
 
-import com.ceos19.everytime.domain.Role;
-import com.ceos19.everytime.domain.School;
-import jakarta.persistence.Column;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import static jakarta.persistence.FetchType.LAZY;
-
 @Getter
 @NoArgsConstructor
-public class AddUserRequest {
+public class JoinUserRequest {
     @NotNull(message = "아이디가 입력되지 않았습니다")
     private String username;
     @NotNull(message = "비밀번호가 입력되지 않았습니다")
@@ -30,5 +21,5 @@ public class AddUserRequest {
     @Email(message = "잘못된 이메일 형식입니다")
     private String email;
     @NotNull(message = "Role이 입력되지 않았습니다")
-    private Role role;
+    private String role;
 }
