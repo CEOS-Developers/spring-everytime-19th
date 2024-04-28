5fad8d571ab85976598216eb6816cdf38c628d90
kwon <kckc0608@naver.com>
2024-04-28 22:26:54 +0900

    feat: 패스워드 해싱 빈 추가

--- src/main/java/com/ceos19/springeverytime/global/config/SecurityConfig.java
+++ src/main/java/com/ceos19/springeverytime/global/config/SecurityConfig.java
@@ -4,11 +4,20 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder () {
+        /**
+         * 비밀번호 암호화를 위한 빈
+         * */
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
