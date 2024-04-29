5d2376b6f45ab664adc5c41ece58f9582f99ec9f
Min Jeon <mulsoap0504@gmail.com>
2024-04-30 01:16:35 +0900

    feat: 채팅방 기능 구현 완료

--- src/main/java/com/ceos19/everytime/controller/api/ChattingRoomController.java
+++ src/main/java/com/ceos19/everytime/controller/api/ChattingRoomController.java
@@ -1,23 +1,25 @@
 package com.ceos19.everytime.controller.api;
 
-import com.ceos19.everytime.dto.AddChattingRoomRequest;
-import com.ceos19.everytime.dto.BaseResponse;
+import com.ceos19.everytime.domain.ChattingRoom;
+import com.ceos19.everytime.dto.*;
 import com.ceos19.everytime.exception.AppException;
+import com.ceos19.everytime.service.ChatService;
 import com.ceos19.everytime.service.ChattingRoomService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/chattingRoom")
 @RequiredArgsConstructor
 public class ChattingRoomController {
     private final ChattingRoomService chattingRoomService;
+    private final ChatService chatService;
 
     @PostMapping
     public ResponseEntity<BaseResponse> addChattingRoom(@Valid @RequestBody AddChattingRoomRequest request) {
@@ -25,6 +27,62 @@ public class ChattingRoomController {
             Long id = chattingRoomService.addChattingRoom(request);
 
             return ResponseEntity.ok(new BaseResponse<>(HttpStatus.OK, null, id, 1));
+        } catch (AppException e) {
+            BaseResponse response =
+                    new BaseResponse(e.getErrorCode().getHttpStatus(), e.getMessage(), null, 0);
+            return new ResponseEntity<>(response, e.getErrorCode().getHttpStatus());
+        }
+    }
+
+    @GetMapping("/{chattingRoom_id}")
+    public ResponseEntity<BaseResponse<ReadChattingRoomResponse>> readChattingRoom(@PathVariable("chattingRoom_id") Long chattingRoomId) {
+        try {
+            ChattingRoom chattingRoom = chattingRoomService.findChattingRoomById(chattingRoomId);
+            ReadChattingRoomResponse value = ReadChattingRoomResponse.from(chattingRoom);
+
+            return ResponseEntity.ok(new BaseResponse<>(HttpStatus.OK, null, value, 1));
+        } catch (AppException e) {
+            BaseResponse response =
+                    new BaseResponse(e.getErrorCode().getHttpStatus(), e.getMessage(), null, 0);
+            return new ResponseEntity<>(response, e.getErrorCode().getHttpStatus());
+        }
+    }
+
+    @PostMapping("/{chattingRoom_id}/chat")
+    public ResponseEntity<BaseResponse> addChat(@PathVariable("chattingRoom_id") Long chattingRoomId, @Valid @RequestBody AddChatRequest request) {
+        try {
+            Long id = chatService.addChat(chattingRoomId, request);
+
+            return ResponseEntity.ok(new BaseResponse<>(HttpStatus.OK, null, id, 1));
+        } catch (AppException e) {
+            BaseResponse response =
+                    new BaseResponse(e.getErrorCode().getHttpStatus(), e.getMessage(), null, 0);
+            return new ResponseEntity<>(response, e.getErrorCode().getHttpStatus());
+        }
+    }
+
+    @GetMapping("/{chattingRoom_id}/chats")
+    public ResponseEntity<BaseResponse<List<ReadChatResponse>>> readChats(@PathVariable("chattingRoom_id") Long chattingRoomId) {
+        try {
+            List<ReadChatResponse> value = new ArrayList<>();
+            chatService.findChatByChattingRoomId(chattingRoomId).forEach(chat -> {
+                value.add(ReadChatResponse.from(chat));
+            });
+
+            return ResponseEntity.ok(new BaseResponse<>(HttpStatus.OK, null, value, value.size()));
+        } catch (AppException e) {
+            BaseResponse response =
+                    new BaseResponse(e.getErrorCode().getHttpStatus(), e.getMessage(), null, 0);
+            return new ResponseEntity<>(response, e.getErrorCode().getHttpStatus());
+        }
+    }
+
+    @DeleteMapping("/{chattingRoom_id}")
+    public ResponseEntity<BaseResponse> deleteChat(@PathVariable("chattingRoom_id") Long chattingRoomId) {
+        try {
+            chattingRoomService.removeChattingRoom(chattingRoomId);
+
+            return ResponseEntity.ok(new BaseResponse<>(HttpStatus.OK, null, null, 0));
         } catch (AppException e) {
             BaseResponse response =
                     new BaseResponse(e.getErrorCode().getHttpStatus(), e.getMessage(), null, 0);
