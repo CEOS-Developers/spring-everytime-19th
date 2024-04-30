72753f58ebcc64ab48d8d1c3aed39a58e6a3557d
JEONG KI MIN <97235034+nimikgnoej@users.noreply.github.com>
2024-04-30 21:09:09 +0900

    [feat] Security 설정파일

---
+++ src/main/java/com/ceos19/springboot/global/config/WebSecurityConfig.java
@@ -0,0 +1,95 @@
+package com.ceos19.springboot.global.config;
+
+import com.ceos19.springboot.jwt.TokenProvider;
+import com.ceos19.springboot.jwt.handler.JwtAuthenticationFilter;
+import com.ceos19.springboot.jwt.handler.JwtExceptionHandlerFilter;
+import com.ceos19.springboot.users.service.UserDetailsServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
+
+@EnableWebSecurity
+@Configuration
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+    private final AuthenticationConfiguration authenticationConfiguration;
+    private final TokenProvider tokenProvider;
+    private final UserDetailsServiceImpl userDetailsService;
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() throws Exception {
+        //AuthenticationManager authenticationManager = authenticationManager(authenticationConfiguration);
+        JwtAuthenticationFilter filter = new JwtAuthenticationFilter(tokenProvider);
+        filter.setAuthenticationManager(authenticationManager(authenticationConfiguration));
+        return filter;
+    }
+    @Bean
+    public JwtExceptionHandlerFilter jwtExceptionHandlerFilter() {
+        return new JwtExceptionHandlerFilter(tokenProvider,userDetailsService);
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedOrigins(List.of("http://localhost:8080"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setExposedHeaders(List.of("*"));
+        //configuration.setMaxAge(3600L); // 1시간 동안 pre-flight 요청을 캐시
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration); // 모든 경로에 대해 설정 적용
+        return source;
+    }
+
+    @Bean
+    public BCryptPasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.cors(corsConfigurer -> corsConfigurer.configurationSource(corsConfigurationSource()))
+                .csrf(CsrfConfigurer::disable)
+                .sessionManagement(configurer -> configurer.sessionCreationPolicy(
+                        SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(
+                        requests -> requests.requestMatchers("/", "/api/user/join"
+                                ,"/api/user/login"
+                                ,"/swagger-ui/index.html"
+                                ,"/swagger-ui/**"
+                                ,"/css/**"
+                                ,"/img/**"
+                                ,"/swagger-resources/**"
+                                ,"/v3/api-docs/**"
+                        ).permitAll()
+                                .anyRequest().hasRole("USER")
+                );
+        http.addFilterBefore(jwtExceptionHandlerFilter(), JwtAuthenticationFilter.class);
+        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
