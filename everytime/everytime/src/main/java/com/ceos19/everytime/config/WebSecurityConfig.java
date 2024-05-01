074e1d033c402976ba56d4fbd1d56f7b19d0effc
chlolive <otatop5819@gmail.com>
2024-05-01 18:17:38 +0900

    Feat: add Spring Security, JWT

---
+++ everytime/everytime/src/main/java/com/ceos19/everytime/config/WebSecurityConfig.java
@@ -0,0 +1,36 @@
+package com.ceos19.everytime.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+
+
+    private final JwtExceptionHandlerFilter jwtExceptionHandlerFilter;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.cors(corsConfigurer -> corsConfigurer.configurationSource(
+                        corsConfigurationSource()))
+                .csrf(CsrfConfigurer::disable)
+                .sessionManagement(configurer -> configurer.sessionCreationPolicy(
+                        SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(
+                        requests -> requests.requestMatchers("/login")
+                                .permitAll());
+
+        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        http.addFilterBefore(jwtExceptionHandlerFilter, JwtAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+}
---
+++ everytime/everytime/src/main/java/com/ceos19/everytime/config/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,4 @@
+package com.ceos19.everytime.config.jwt;
+
+public class JwtAuthenticationFilter {
+}
