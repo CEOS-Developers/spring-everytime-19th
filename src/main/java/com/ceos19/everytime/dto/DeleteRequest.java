4bbbe802f93205acb86e7830446ffc826834df01
parking0 <p980710@gmail.com>
2024-05-03 16:45:29 +0900

    refactor : MessageController에 @AuthenticationPrincipal 적용

--- src/main/java/com/ceos19/everytime/dto/DeleteRequest.java
+++ src/main/java/com/ceos19/everytime/dto/DeleteRequest.java
@@ -1,14 +1,2 @@
-package com.ceos19.everytime.dto;
-
-import jakarta.validation.constraints.NotNull;
-import lombok.*;
-
-@Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
-@ToString
-public class DeleteRequest {
-
-    @NotNull(message = "회원 아이디를 적어주세요.")
-    private Long memberId;
+package com.ceos19.everytime.dto;public class DeleteRequest {
 }
