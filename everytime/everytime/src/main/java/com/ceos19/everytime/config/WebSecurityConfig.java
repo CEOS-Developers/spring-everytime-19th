4ff28951abfb20efa3a12cebdfecc7c681c15fe0
chlolive <otatop5819@gmail.com>
2024-05-03 19:48:30 +0900

    Feat: JWT TokenProvider update

--- everytime/everytime/src/main/java/com/ceos19/everytime/config/WebSecurityConfig.java
+++ everytime/everytime/src/main/java/com/ceos19/everytime/config/WebSecurityConfig.java
@@ -1,10 +1,12 @@
 package com.ceos19.everytime.config;
 
+import com.ceos19.everytime.config.jwt.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
