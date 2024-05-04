570c2a393b4de690882649d67d00b3a4fb2410b6
yeong-hwan <jghff700@naver.com>
2024-05-05 00:50:22 +0900

    refactor(user): append authority and activate state

--- src/main/java/com/ceos19/springboot/domain/User.java
+++ src/main/java/com/ceos19/springboot/domain/User.java
@@ -1,4 +1,5 @@
 package com.ceos19.springboot.domain;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import lombok.Builder;
@@ -6,6 +7,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.Set;
 
 @Entity
 @Getter
@@ -44,6 +46,18 @@ public class User extends BaseTimeEntity {
     @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     @JoinColumn(name = "school_id")
     private School school;
+
+    @JsonIgnore
+    @Column(name = "activated")
+    private boolean activated;
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_authority",
+            joinColumns = {@JoinColumn(name = "id", referencedColumnName = "id")},
+            inverseJoinColumns = {@JoinColumn(name = "authority_name", referencedColumnName = "authority_name")})
+    private Set<Authority> authorities;
+
 
     @Builder
     public User(String username, String password, String nickname,
