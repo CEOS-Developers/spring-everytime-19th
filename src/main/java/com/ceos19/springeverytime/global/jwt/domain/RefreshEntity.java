a1521c0a081055989966d8a321644d3e7e1c72f2
hyeongjun im <limbs713@naver.com>
2024-05-04 21:49:34 +0900

    :sparkles: 추가 RefreshEntity

---
+++ src/main/java/com/ceos19/springeverytime/global/jwt/domain/RefreshEntity.java
@@ -0,0 +1,27 @@
+package com.ceos19.springeverytime.global.jwt.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class RefreshEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String refresh;
+    private String expiration;
+}
