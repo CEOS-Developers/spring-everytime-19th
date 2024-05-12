86034de64189410429f4c7fcfb0b8e22ed920b40
kckc0608 <kckc0608@gmail.com>
2024-05-12 12:31:38 +0900

    test 코드 작성

--- src/test/java/com/ceos19/springeverytime/service/UserServiceTest.java
+++ src/test/java/com/ceos19/springeverytime/service/UserServiceTest.java
@@ -3,6 +3,7 @@ package com.ceos19.springeverytime.service;
 import com.ceos19.springeverytime.common.EntityGenerator;
 import com.ceos19.springeverytime.domain.user.domain.User;
 import com.ceos19.springeverytime.domain.user.dto.request.UserCreateRequest;
+import com.ceos19.springeverytime.domain.user.dto.response.CustomUserDetails;
 import com.ceos19.springeverytime.domain.user.repository.UserRepository;
 import com.ceos19.springeverytime.domain.user.service.UserService;
 import org.assertj.core.api.Assertions;
@@ -12,10 +13,20 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.test.context.support.WithMockUser;
 
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
@@ -54,11 +65,21 @@ public class UserServiceTest {
     }
 
     @Test
+    @WithMockUser(username = "test")
     @DisplayName("유저 회원 탈퇴 테스트")
     void 회원탈퇴_테스트() {
         // given
+        User user = EntityGenerator.generateUser("test");
+        UserDetails userDetails = new CustomUserDetails(user);
+        UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(userDetails, null);
+        SecurityContextHolder.getContext().setAuthentication(auth);
+
+        when(userRepository.findByLoginId(anyString())).thenReturn(Optional.of(user));
+
         // when
+        userService.delete(auth);
 
         // then
+        verify(userRepository, times(1)).delete(user);
     }
 }
