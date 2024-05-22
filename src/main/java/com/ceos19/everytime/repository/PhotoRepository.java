36e427812ec4e655e11082eb20a1924d2cda1415
이윤서 <90557277+yoonsseo@users.noreply.github.com>
2024-05-22 10:55:04 +0900

    Revert "[19기_박시영] Github Action을 이용한 CI/CD 배포 미션 제출합니다."

--- src/main/java/com/ceos19/everytime/repository/PhotoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ceos19.everytime.repository;
-
-import com.ceos19.everytime.domain.Photo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PhotoRepository extends JpaRepository<Photo, Long> {
-}
